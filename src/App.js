b7cafefdf4e8eeb701f2547c899e75cde80a2a8c
Marcus Koh <kohchihao@gmail.com>
2020-03-05 17:44:07 +0800

    Update App.js

--- src/App.js
+++ src/App.js
@@ -16,7 +16,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Learn React1
         </a>
       </header>
     </div>
