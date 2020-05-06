e4e2c3ecf4203dd0e6b459f90ee57c054c847849
Marcus Koh <kohchihao@gmail.com>
2020-05-06 21:11:52 +0800

    Update deploy-dokku.sh

--- config/travis/deploy-dokku.sh
+++ config/travis/deploy-dokku.sh
@@ -5,4 +5,5 @@ ssh-add .travis/dokku-deploy.key;
 ssh-keyscan "$DOKKU_HOST" >> ~/.ssh/known_hosts;
 git remote add deploy dokku@"$DOKKU_HOST":"$DOKKU_APP";
 git config --global push.default simple;
+git fetch --unshallow;
 git push deploy master;
