4551de7fdfa5aaa26a58b3814a88dccd10441e81
Jin Ying <kenneth.jyt@gmail.com>
2020-05-07 01:05:59 +0800

    Add CategoriesAPI skeleton

---
+++ utils/api/categories.js
@@ -0,0 +1,26 @@
+class CategoriesAPI {
+  /**
+   * Get all the categories
+   * @throws {FirestoreError} 
+   * @return {array} A list of all the categories
+   */
+  async getAll() {}
+
+  /**
+   * Get a category by its id
+   * @param {string} id The category id
+   * @throws {FirestoreError}
+   * @return {object} The category's info
+   */
+  async get(id) {}
+
+  /**
+   * Get a category by its name
+   * @param {string} name The category name
+   * @throws {FirestoreError}
+   * @return {object} The category's info
+   */
+  async get(name) {}
+}
+
+export default CategoriesAPI;
