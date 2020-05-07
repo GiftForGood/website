4854a5cfceefdf6a3a643eb2c77a4bf3a5ff9a7d
Jin Ying <kenneth.jyt@gmail.com>
2020-05-07 20:33:17 +0800

    Change 'useTabs' to false in prettier and re-run fomatter

--- utils/api/donations.js
+++ utils/api/donations.js
@@ -1,153 +1,153 @@
 class DonationsAPI {
-	#pendingDonationsForCategoryQuery;
-	#prevPendingDonationsForCategoryDoc;
-	#donorDonationsQuery;
-	#prevDonorDonationsDoc;
-	#donorDonationsByStatusQuery;
-	#prevDonorDonationsByStatusDoc;
-	#completedNPODonationsQuery;
-	#prevCompletedNPODonationsDoc;
+  #pendingDonationsForCategoryQuery;
+  #prevPendingDonationsForCategoryDoc;
+  #donorDonationsQuery;
+  #prevDonorDonationsDoc;
+  #donorDonationsByStatusQuery;
+  #prevDonorDonationsByStatusDoc;
+  #completedNPODonationsQuery;
+  #prevCompletedNPODonationsDoc;
 
-	/**
-	 * Create a new donation
-	 * @param {string} title The donation title
-	 * @param {string} description The donation description
-	 * @param {array} categories A list of categories that the donation belongs to
-	 * @param {object} validPeriodFrom The valid date that the donation is valid from
-	 * @param {object} validPeriodTo The valid date that the donation is valid to
-	 * @param {string} dimensions The dimension of the donated item
-	 * @param {string} location The location of the donation
-	 * @param {string} itemCondition The condition of the donated item
-	 * @param {string} coverImagePath The path of the cover image for the donation
-	 * @param {array} imagesPath A list of path of the images for the donations. Should include the cover image path
-	 * @return {boolean} true if the donation is created, false otherwise
-	 */
-	async create(
-		title,
-		description,
-		categories,
-		validPeriodFrom,
-		validPeriodTo,
-		dimensions,
-		location,
-		itemCondition,
-		coverImagePath,
-		imagesPath
-	) {}
+  /**
+   * Create a new donation
+   * @param {string} title The donation title
+   * @param {string} description The donation description
+   * @param {array} categories A list of categories that the donation belongs to
+   * @param {object} validPeriodFrom The valid date that the donation is valid from
+   * @param {object} validPeriodTo The valid date that the donation is valid to
+   * @param {string} dimensions The dimension of the donated item
+   * @param {string} location The location of the donation
+   * @param {string} itemCondition The condition of the donated item
+   * @param {string} coverImagePath The path of the cover image for the donation
+   * @param {array} imagesPath A list of path of the images for the donations. Should include the cover image path
+   * @return {boolean} true if the donation is created, false otherwise
+   */
+  async create(
+    title,
+    description,
+    categories,
+    validPeriodFrom,
+    validPeriodTo,
+    dimensions,
+    location,
+    itemCondition,
+    coverImagePath,
+    imagesPath
+  ) {}
 
-	/**
-	 * Search donations containing the text both in the title and description
-	 * @param {string} text
-	 * @throws {FirestoreError}
-	 * @return {array} A list of donations that contain the text
-	 */
-	async search(text) {}
+  /**
+   * Search donations containing the text both in the title and description
+   * @param {string} text
+   * @throws {FirestoreError}
+   * @return {array} A list of donations that contain the text
+   */
+  async search(text) {}
 
-	/**
-	 * Get the top X pending donations belonging to a category, sorted by timestamp
-	 * @param {string} category The category name
-	 * @param {number} limit The number of donations to return
-	 * @throws {FirestoreError}
-	 * @returns {array} A list of X pending donations, sorted by timestamp
-	 */
-	async getTopXPendingDonations(category, limit) {}
+  /**
+   * Get the top X pending donations belonging to a category, sorted by timestamp
+   * @param {string} category The category name
+   * @param {number} limit The number of donations to return
+   * @throws {FirestoreError}
+   * @returns {array} A list of X pending donations, sorted by timestamp
+   */
+  async getTopXPendingDonations(category, limit) {}
 
-	/**
-	 * Get the initial batch of pending donations belonging to a category. Only return DONATIONS_BATCH_SIZE results
-	 * @param {string} category The category's name
-	 * @param {string} orderBy The way to order the donations
-	 * @throws {FirestoreError}
-	 * @return {array} A list of ordered donations belonging to a category
-	 */
-	async getInitialBatchOfPendingDonationsForCategory(category, orderBy) {}
+  /**
+   * Get the initial batch of pending donations belonging to a category. Only return DONATIONS_BATCH_SIZE results
+   * @param {string} category The category's name
+   * @param {string} orderBy The way to order the donations
+   * @throws {FirestoreError}
+   * @return {array} A list of ordered donations belonging to a category
+   */
+  async getInitialBatchOfPendingDonationsForCategory(category, orderBy) {}
 
-	/**
-	 * Get the next batch of pending donations belonging to a category that was previously queried. Only return DONATIONS_BATCH_SIZE results
-	 * @throws {ReferenceError}
-	 * @throws {FirestoreError}
-	 * @return {array} The ordered donations belonging to a category
-	 */
-	async getNextBatchOfPendingDonationsForCategory() {}
+  /**
+   * Get the next batch of pending donations belonging to a category that was previously queried. Only return DONATIONS_BATCH_SIZE results
+   * @throws {ReferenceError}
+   * @throws {FirestoreError}
+   * @return {array} The ordered donations belonging to a category
+   */
+  async getNextBatchOfPendingDonationsForCategory() {}
 
-	/**
-	 * Get a donation by its id
-	 * @param {string} id
-	 * @throws {FirestoreError}
-	 * @return {object} The donation info
-	 */
-	async getDonation(id) {}
+  /**
+   * Get a donation by its id
+   * @param {string} id
+   * @throws {FirestoreError}
+   * @return {object} The donation info
+   */
+  async getDonation(id) {}
 
-	/**
-	 * Get the initial batch of donations belonging to a donor. Only return DONATIONS_BATCH_SIZE results
-	 * @param {string} donorID
-	 * @return {array} A list of donations belonging to a NPO
-	 */
-	async getInitialBatchOfDonorDonations(donorID) {}
+  /**
+   * Get the initial batch of donations belonging to a donor. Only return DONATIONS_BATCH_SIZE results
+   * @param {string} donorID
+   * @return {array} A list of donations belonging to a NPO
+   */
+  async getInitialBatchOfDonorDonations(donorID) {}
 
-	/**
-	 * Get the next batch of donor donations that was previously queried. Only return DONATIONS_BATCH_SIZE results
-	 * @throws {ReferenceError}
-	 * @throws {FirestoreError}
-	 * @return {array} A list of donations belonging to a donor
-	 */
-	async getNextBatchOfDonorDonations() {}
+  /**
+   * Get the next batch of donor donations that was previously queried. Only return DONATIONS_BATCH_SIZE results
+   * @throws {ReferenceError}
+   * @throws {FirestoreError}
+   * @return {array} A list of donations belonging to a donor
+   */
+  async getNextBatchOfDonorDonations() {}
 
-	/**
-	 * Get the initial batch of donations belonging to a donor filter by its status. Only return DONATIONS_BATCH_SIZE results
-	 * @param {string} npoID
-	 * @param {string} status The status of the donations
-	 * @throws {FirestoreError}
-	 * @return {array} A list of donations belonging donor, filtered by status
-	 */
-	async getInitialBatchOfDonorDonationsFilterByStatus(donorID, status) {}
+  /**
+   * Get the initial batch of donations belonging to a donor filter by its status. Only return DONATIONS_BATCH_SIZE results
+   * @param {string} npoID
+   * @param {string} status The status of the donations
+   * @throws {FirestoreError}
+   * @return {array} A list of donations belonging donor, filtered by status
+   */
+  async getInitialBatchOfDonorDonationsFilterByStatus(donorID, status) {}
 
-	/**
-	 * Get the next batch of donations belonging to a donor filter by its status. Only return DONATIONS_BATCH_SIZE results
-	 * @throws {ReferenceError}
-	 * @throws {FirestoreError}
-	 * @return {array} A list of donations belonging to a donor, filtered by status
-	 */
-	async getNextBatchOfDonorDonationsFilterByStatus() {}
+  /**
+   * Get the next batch of donations belonging to a donor filter by its status. Only return DONATIONS_BATCH_SIZE results
+   * @throws {ReferenceError}
+   * @throws {FirestoreError}
+   * @return {array} A list of donations belonging to a donor, filtered by status
+   */
+  async getNextBatchOfDonorDonationsFilterByStatus() {}
 
-	/**
-	 * Get the initial batch of donations that are completed by a NPO. Only return DONATIONS_BATCH_SIZE results
-	 * @param {string} npoID
-	 * @throws {FirestoreError}
-	 * @return {array} A list of donations that are completed by a NPO
-	 */
-	async getInitialBatchOfNPOCompletedDonations(npoID) {}
+  /**
+   * Get the initial batch of donations that are completed by a NPO. Only return DONATIONS_BATCH_SIZE results
+   * @param {string} npoID
+   * @throws {FirestoreError}
+   * @return {array} A list of donations that are completed by a NPO
+   */
+  async getInitialBatchOfNPOCompletedDonations(npoID) {}
 
-	/**
-	 * Get the next batch of donations completed by a NPO that was previously queried. Only return DONATIONS_BATCH_SIZE results
-	 * @throws {ReferenceError}
-	 * @throws {FirestoreError}
-	 * @return {array} A list of donations that are completed by a NPO
-	 */
-	async getNextBatchOfNPOCompletedDonations() {}
+  /**
+   * Get the next batch of donations completed by a NPO that was previously queried. Only return DONATIONS_BATCH_SIZE results
+   * @throws {ReferenceError}
+   * @throws {FirestoreError}
+   * @return {array} A list of donations that are completed by a NPO
+   */
+  async getNextBatchOfNPOCompletedDonations() {}
 
-	/**
-	 * Update the fields of a donation. Does not include updating of status
-	 * @param {string} id The donation id
-	 * @param {object} data The data to update to
-	 * @return {boolean} true if the donation field/s is updated, false otherwise
-	 */
-	async updateDonation(id, data) {}
+  /**
+   * Update the fields of a donation. Does not include updating of status
+   * @param {string} id The donation id
+   * @param {object} data The data to update to
+   * @return {boolean} true if the donation field/s is updated, false otherwise
+   */
+  async updateDonation(id, data) {}
 
-	/**
-	 * Close a donation
-	 * @param {string} id The donation id
-	 * @param {string} reason The reason for closing
-	 * @return {boolean} true if the donation is closed successfully, false otherwise
-	 */
-	async closeDonation(id, reason) {}
+  /**
+   * Close a donation
+   * @param {string} id The donation id
+   * @param {string} reason The reason for closing
+   * @return {boolean} true if the donation is closed successfully, false otherwise
+   */
+  async closeDonation(id, reason) {}
 
-	/**
-	 * Complete a donation
-	 * @param {string} id The donation id
-	 * @param {string} npoID The NPO id that completed the donation
-	 * @return {boolean} true if the donation is completed successfully, false otherwise
-	 */
-	async completeDonation(id, npoID) {}
+  /**
+   * Complete a donation
+   * @param {string} id The donation id
+   * @param {string} npoID The NPO id that completed the donation
+   * @return {boolean} true if the donation is completed successfully, false otherwise
+   */
+  async completeDonation(id, npoID) {}
 }
 
 export default DonationsAPI;
--- utils/api/wishes.js
+++ utils/api/wishes.js
@@ -1,142 +1,142 @@
 class WishesAPI {
-	#pendingWishesForCategoryQuery;
-	#prevPendingWishesForCategoryDoc;
-	#npoWishesQuery;
-	#prevNPOWishesDoc;
-	#npoWishesByStatusQuery;
-	#prevNPOWishesByStatusDoc;
-	#completedDonorWishesQuery;
-	#prevCompletedDonorWishesDoc;
+  #pendingWishesForCategoryQuery;
+  #prevPendingWishesForCategoryDoc;
+  #npoWishesQuery;
+  #prevNPOWishesDoc;
+  #npoWishesByStatusQuery;
+  #prevNPOWishesByStatusDoc;
+  #completedDonorWishesQuery;
+  #prevCompletedDonorWishesDoc;
 
-	/**
-	 * Create a new wish
-	 * @param {string} title The wish title
-	 * @param {string} description The wish description
-	 * @param {array} categories A list of categories that the wish belongs to
-	 * @return {boolean} true if the wish is created, false otherwise
-	 */
-	async create(title, description, categories) {}
+  /**
+   * Create a new wish
+   * @param {string} title The wish title
+   * @param {string} description The wish description
+   * @param {array} categories A list of categories that the wish belongs to
+   * @return {boolean} true if the wish is created, false otherwise
+   */
+  async create(title, description, categories) {}
 
-	/**
-	 * Search wishes containing the text both in the title and description
-	 * @param {string} text
-	 * @throws {FirestoreError}
-	 * @return {array} A list of wishes that contain the text
-	 */
-	async search(text) {}
+  /**
+   * Search wishes containing the text both in the title and description
+   * @param {string} text
+   * @throws {FirestoreError}
+   * @return {array} A list of wishes that contain the text
+   */
+  async search(text) {}
 
-	/**
-	 * Get the top X pending wishes belonging to a category, sorted by timestamp
-	 * @param {string} category The category name
-	 * @param {number} limit The number of wishes to return
-	 * @throws {FirestoreError}
-	 * @return {array} A list of X pending wishes, sorted by timestamp
-	 */
-	async getTopXPendingWishes(category, limit) {}
+  /**
+   * Get the top X pending wishes belonging to a category, sorted by timestamp
+   * @param {string} category The category name
+   * @param {number} limit The number of wishes to return
+   * @throws {FirestoreError}
+   * @return {array} A list of X pending wishes, sorted by timestamp
+   */
+  async getTopXPendingWishes(category, limit) {}
 
-	/**
-	 * Get the initial batch of pending wishes belonging to a category. Only return WISHES_BATCH_SIZE results
-	 * @param {string} category The category's name
-	 * @param {string} orderBy The way to order the wishes
-	 * @throws {FirestoreError}
-	 * @return {array} A list of ordered wishes belonging to a category
-	 */
-	async getInitialBatchOfPendingWishesForCategory(category, orderBy) {}
+  /**
+   * Get the initial batch of pending wishes belonging to a category. Only return WISHES_BATCH_SIZE results
+   * @param {string} category The category's name
+   * @param {string} orderBy The way to order the wishes
+   * @throws {FirestoreError}
+   * @return {array} A list of ordered wishes belonging to a category
+   */
+  async getInitialBatchOfPendingWishesForCategory(category, orderBy) {}
 
-	/**
-	 * Get the next batch of pending wishes belonging to a category that was previously queried. Only return WISHES_BATCH_SIZE results
-	 * @throws {ReferenceError}
-	 * @throws {FirestoreError}
-	 * @return {array} The ordered wishes belonging to a category
-	 */
-	async getNextBatchOfPendingWishesForCategory() {}
+  /**
+   * Get the next batch of pending wishes belonging to a category that was previously queried. Only return WISHES_BATCH_SIZE results
+   * @throws {ReferenceError}
+   * @throws {FirestoreError}
+   * @return {array} The ordered wishes belonging to a category
+   */
+  async getNextBatchOfPendingWishesForCategory() {}
 
-	/**
-	 * Get a wish by its id
-	 * @param {string} id The wish id
-	 * @throws {FirestoreError}
-	 * @return {object} The wish info
-	 */
-	async getWish(id) {}
+  /**
+   * Get a wish by its id
+   * @param {string} id The wish id
+   * @throws {FirestoreError}
+   * @return {object} The wish info
+   */
+  async getWish(id) {}
 
-	/**
-	 * Get the initial batch of wishes belonging to a NPO. Only return WISHES_BATCH_SIZE results
-	 * @param {string} npoID
-	 * @return {array} A list of wishes belonging to a NPO
-	 */
-	async getInitialBatchOfNPOWishes(npoID) {}
+  /**
+   * Get the initial batch of wishes belonging to a NPO. Only return WISHES_BATCH_SIZE results
+   * @param {string} npoID
+   * @return {array} A list of wishes belonging to a NPO
+   */
+  async getInitialBatchOfNPOWishes(npoID) {}
 
-	/**
-	 * Get the next batch of NPO wishes that was previously queried. Only return WISHES_BATCH_SIZE results
-	 * @throws {ReferenceError}
-	 * @throws {FirestoreError}
-	 * @return {array} A list of wishes belonging to a NPO
-	 */
-	async getNextBatchOfNPOWishes() {}
+  /**
+   * Get the next batch of NPO wishes that was previously queried. Only return WISHES_BATCH_SIZE results
+   * @throws {ReferenceError}
+   * @throws {FirestoreError}
+   * @return {array} A list of wishes belonging to a NPO
+   */
+  async getNextBatchOfNPOWishes() {}
 
-	/**
-	 * Get the initial batch of wishes belonging to a NPO filter by its status. Only return WISHES_BATCH_SIZE results
-	 * @param {string} npoID
-	 * @param {string} status The status of the wishes
-	 * @throws {FirestoreError}
-	 * @return {array} A list of wishes belonging NPO, filtered by status
-	 */
-	async getInitialBatchOfNPOWishesFilterByStatus(npoID, status) {}
+  /**
+   * Get the initial batch of wishes belonging to a NPO filter by its status. Only return WISHES_BATCH_SIZE results
+   * @param {string} npoID
+   * @param {string} status The status of the wishes
+   * @throws {FirestoreError}
+   * @return {array} A list of wishes belonging NPO, filtered by status
+   */
+  async getInitialBatchOfNPOWishesFilterByStatus(npoID, status) {}
 
-	/**
-	 * Get the next batch of wishes belonging to a NPO filter by its status. Only return WISHES_BATCH_SIZE results
-	 * @throws {ReferenceError}
-	 * @throws {FirestoreError}
-	 * @return {array} A list of wishes belonging to a NPO, filtered by status
-	 */
-	async getNextBatchOfNPOWishesFilterByStatus() {}
+  /**
+   * Get the next batch of wishes belonging to a NPO filter by its status. Only return WISHES_BATCH_SIZE results
+   * @throws {ReferenceError}
+   * @throws {FirestoreError}
+   * @return {array} A list of wishes belonging to a NPO, filtered by status
+   */
+  async getNextBatchOfNPOWishesFilterByStatus() {}
 
-	/**
-	 * Get the initial batch of wishes that are completed by a donor. Only return WISHES_BATCH_SIZE results
-	 * @param {string} donorID
-	 * @throws {FirestoreError}
-	 * @return {array} A list of wishes that are completed by a donor
-	 */
-	async getInitialBatchOfDonorCompletedWishes(donorID) {}
+  /**
+   * Get the initial batch of wishes that are completed by a donor. Only return WISHES_BATCH_SIZE results
+   * @param {string} donorID
+   * @throws {FirestoreError}
+   * @return {array} A list of wishes that are completed by a donor
+   */
+  async getInitialBatchOfDonorCompletedWishes(donorID) {}
 
-	/**
-	 * Get the next batch of wishes completed by a donor that was previously queried. Only return WISHES_BATCH_SIZE results
-	 * @throws {ReferenceError}
-	 * @throws {FirestoreError}
-	 * @return {array} A list of wishes that are completed by a donor
-	 */
-	async getNextBatchOfDonorCompletedWishes() {}
+  /**
+   * Get the next batch of wishes completed by a donor that was previously queried. Only return WISHES_BATCH_SIZE results
+   * @throws {ReferenceError}
+   * @throws {FirestoreError}
+   * @return {array} A list of wishes that are completed by a donor
+   */
+  async getNextBatchOfDonorCompletedWishes() {}
 
-	/**
-	 * Update the fields of a wish. Does not include updating of status
-	 * @param {string} id The wish id
-	 * @param {object} data The data to update to
-	 * @return {boolean} true if the wish field/s is updated, false otherwise
-	 */
-	async updateWish(id, data) {}
+  /**
+   * Update the fields of a wish. Does not include updating of status
+   * @param {string} id The wish id
+   * @param {object} data The data to update to
+   * @return {boolean} true if the wish field/s is updated, false otherwise
+   */
+  async updateWish(id, data) {}
 
-	/**
-	 * Bump a wish
-	 * @param {string} id The wish id
-	 * @return {boolean} true if the wish is bumped successfully, false otherwise
-	 */
-	async bumpWish(id) {}
+  /**
+   * Bump a wish
+   * @param {string} id The wish id
+   * @return {boolean} true if the wish is bumped successfully, false otherwise
+   */
+  async bumpWish(id) {}
 
-	/**
-	 * Close a wish
-	 * @param {string} id The wish id
-	 * @param {string} reason The reason for closing
-	 * @return {boolean} true if the wish is closed successfully, false otherwise
-	 */
-	async closeWish(id, reason) {}
+  /**
+   * Close a wish
+   * @param {string} id The wish id
+   * @param {string} reason The reason for closing
+   * @return {boolean} true if the wish is closed successfully, false otherwise
+   */
+  async closeWish(id, reason) {}
 
-	/**
-	 * Complete a wish
-	 * @param {string} id The wish id
-	 * @param {string} donorID The donor id that completed the wish
-	 * @return {boolean} true if the wish is completed successfully, false otherwise
-	 */
-	async completeWish(id, donorID) {}
+  /**
+   * Complete a wish
+   * @param {string} id The wish id
+   * @param {string} donorID The donor id that completed the wish
+   * @return {boolean} true if the wish is completed successfully, false otherwise
+   */
+  async completeWish(id, donorID) {}
 }
 
 export default WishesAPI;
