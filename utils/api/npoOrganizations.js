9cf9bf4822927f92bad881d4a529bc9dbccaeaea
Jin Ying <kenneth.jyt@gmail.com>
2020-05-07 20:58:01 +0800

    Add NPOOrganizationAPI skeleton

---
+++ utils/api/npoOrganizations.js
@@ -0,0 +1,18 @@
+class NPOOrganizationAPI {
+  /**
+   * Get all the NPO Organizations
+   * @throws {FirestoreError}
+   * @return {array} A list of all the npo organizations
+   */
+  async getAll() {}
+
+  /**
+   * Get a NPO organization info by it's name
+   * @param {string} name The NPO organization name
+   * @throws {FirestoreError}
+   * @return {object} The NPO organization info
+   */
+  async get(name) {}
+}
+
+export default NPOOrganizationAPI;
