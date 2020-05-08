55540859fb721bcee6d316565ed970d627f6dc13
Jin Ying <kenneth.jyt@gmail.com>
2020-05-09 00:07:46 +0800

    Add implementation for CategoriesAPI

--- utils/api/categories.js
+++ utils/api/categories.js
@@ -1,23 +1,33 @@
+import { db } from '../firebase';
+
+const categoriesCollection = db.collection('categories');
+
 class CategoriesAPI {
   /**
    * Get all the categories
    * @return {object} A firebase document of all the categories
    */
-  async getAll() {}
+  async getAll() {
+    return categoriesCollection.get();
+  }
 
   /**
    * Get a category by its id
    * @param {string} id The category id
    * @return {object} A firebase document of the category info
    */
-  async get(id) {}
+  async getById(id) {
+    return categoriesCollection.where('id', '==', id).get();
+  }
 
   /**
    * Get a category by its name
    * @param {string} name The category name
    * @return {object} A firebase document of the category info
    */
-  async get(name) {}
+  async getByName(name) {
+    return categoriesCollection.where('name', '==', name).get();
+  }
 }
 
 export default CategoriesAPI;
