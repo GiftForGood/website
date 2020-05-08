12c78340fc02e236730b02b4da21c16832162e12
Jin Ying <kenneth.jyt@gmail.com>
2020-05-08 23:43:28 +0800

    Add implementation for NPOOrganizationsAPI

--- utils/api/npoOrganizations.js
+++ utils/api/npoOrganizations.js
@@ -1,16 +1,26 @@
+import { db } from '../firebase';
+
+const COLLECTION_NAME = "npoOrganizations";
+
 class NPOOrganizationAPI {
   /**
    * Get all the NPO Organizations
    * @return {object} A firebase document of all the npo organizations
    */
-  async getAll() {}
+  async getAll() {
+    return db.collection(COLLECTION_NAME).get()
+  }
 
   /**
    * Get a NPO organization info by it's name
    * @param {string} name The NPO organization name
    * @return {object} A firebase document of the NPO organization info
    */
-  async get(name) {}
+  async get(name) {
+    return db.collection(COLLECTION_NAME)
+      .where('name', '==', name)
+      .get()
+  }
 }
 
 export default NPOOrganizationAPI;
