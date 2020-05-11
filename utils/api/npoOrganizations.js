8c305383bb1babc8dcebd78893fb4ed8b90f6772
Jin Ying <kenneth.jyt@gmail.com>
2020-05-12 01:38:15 +0800

    api/npoOrganization.js: Update api docs

--- utils/api/npoOrganizations.js
+++ utils/api/npoOrganizations.js
@@ -5,6 +5,7 @@ const npoOrganizationsCollection = db.collection('npoOrganizations');
 class NPOOrganizationAPI {
   /**
    * Get all the NPO Organizations
+   * @throws {FirebaseError}
    * @return {object} A firebase document of all the npo organizations
    */
   async getAll() {
@@ -14,6 +15,7 @@ class NPOOrganizationAPI {
   /**
    * Get a NPO organization info by it's name
    * @param {string} name The NPO organization name
+   * @throws {FirebaseError}
    * @return {object} A firebase document of the NPO organization info
    */
   async get(name) {
