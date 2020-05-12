9ebaed0de6955cd3dc23004d9c78842481c055b2
Marcus Koh <kohchihao@gmail.com>
2020-05-12 10:18:55 +0800

    Add Global Styles

---
+++ pages/_app.js
@@ -0,0 +1,26 @@
+import * as React from 'react'
+import App from 'next/app'
+import { getTokens } from '@kiwicom/orbit-components'
+import { ThemeProvider, createGlobalStyle } from 'styled-components'
+
+const GlobalStyle = createGlobalStyle`
+  body {
+    margin: 0 auto;
+  }
+`
+
+const tokens = getTokens()
+
+export default class MyApp extends App {
+  render() {
+    const { Component, pageProps } = this.props
+    return (
+      <ThemeProvider theme={{ orbit: tokens }}>
+        <>
+          <GlobalStyle />
+          <Component {...pageProps} />
+        </>
+      </ThemeProvider>
+    )
+  }
+}
