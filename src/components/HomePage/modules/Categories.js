06ea110c2ee90fb995e31473f3f64e414489927f
jamessspanggg <jamespangmunwai@gmail.com>
2020-05-13 15:41:51 +0800

    Read categories from firestore

--- src/components/HomePage/modules/Categories.js
+++ src/components/HomePage/modules/Categories.js
@@ -23,79 +23,103 @@ const ScrollableRow = styled.div`
 
 const dummyCategoriesData = [
   {
-    iconUrl: 'url',
-    id: '1234',
-    imageUrl: '/assets/wishes-banner.jpg',
-    name: 'necessities',
+    "iconUrl": "dailynecessities_icon",
+    "name": "Daily Necessities",
+    "imageUrl": "https://res.cloudinary.com/giftforgood/image/upload/v1588842156/dailynecessities.jpg",
+    "id": "3ZurlkyhxGG6jYiuzoKB"
   },
   {
-    iconUrl: 'url',
-    id: '123435',
-    imageUrl: '/assets/wishes-banner.jpg',
-    name: 'furniture',
+    "iconUrl": "stationery_icon",
+    "name": "Stationery",
+    "imageUrl": "https://res.cloudinary.com/giftforgood/image/upload/v1588842198/stationery.jpg",
+    "id": "49yM4ELhrrFWr6phmP8d"
   },
   {
-    iconUrl: 'url',
-    id: '12546543',
-    imageUrl: '/assets/wishes-banner.jpg',
-    name: 'house',
+    "imageUrl": "https://res.cloudinary.com/giftforgood/image/upload/v1588838805/toysandgames.jpg",
+    "id": "GYYRGfzyq67KIj17gVwZ",
+    "name": "Toys and Games",
+    "iconUrl": "toysandgames_icon"
   },
   {
-    iconUrl: 'url',
-    id: '15454623',
-    imageUrl: '/assets/wishes-banner.jpg',
-    name: 'flat',
+    "iconUrl": "fake_url",
+    "name": "Electronics",
+    "imageUrl": "https://res.cloudinary.com/giftforgood/image/upload/v1588842147/electronic.jpg",
+    "id": "IwmfcaTjKqrnviMxHQ5G"
   },
   {
-    iconUrl: 'url',
-    id: '123423423',
-    imageUrl: '/assets/wishes-banner.jpg',
-    name: 'flrewrewdsat',
+    "iconUrl": "food_icon",
+    "name": "Food",
+    "imageUrl": "https://res.cloudinary.com/giftforgood/image/upload/v1588842129/food.jpg",
+    "id": "LokORpW2MEKJx1ayG3h6"
   },
   {
-    iconUrl: 'url',
-    id: '126587683',
-    imageUrl: '/assets/wishes-banner.jpg',
-    name: 'flatdsdfds',
+    "imageUrl": "https://res.cloudinary.com/giftforgood/image/upload/v1588838797/sportequipment.jpg",
+    "id": "QGnaiZt0eoGIMVQZ4qwn",
+    "name": "Sport Equipment",
+    "iconUrl": "N.A."
   },
   {
-    iconUrl: 'url',
-    id: '987',
-    imageUrl: '/assets/wishes-banner.jpg',
-    name: 'dasdsad',
+    "name": "Others",
+    "iconUrl": "N.A.",
+    "imageUrl": "https://res.cloudinary.com/giftforgood/image/upload/v1588838786/other.jpg",
+    "id": "QRniNylLwFLDmQByh9BA"
   },
   {
-    iconUrl: 'url',
-    id: '1298793',
-    imageUrl: '/assets/wishes-banner.jpg',
-    name: 'eqrewq',
+    "id": "bYh4RKtS4grMNIzBsM4o",
+    "name": "Medical Equipment",
+    "iconUrl": "N.A.",
+    "imageUrl": "https://res.cloudinary.com/giftforgood/image/upload/v1588838774/medicalequipment.jpg"
   },
   {
-    iconUrl: 'url',
-    id: '129873',
-    imageUrl: '/assets/wishes-banner.jpg',
-    name: 'dfsf',
+    "id": "fw6cW4v1m8haLe5rSix4",
+    "name": "Apparel",
+    "iconUrl": "fake",
+    "imageUrl": "https://res.cloudinary.com/giftforgood/image/upload/v1588842183/apparel.jpg"
   },
   {
-    iconUrl: 'url',
-    id: '12798873',
-    imageUrl: '/assets/wishes-banner.jpg',
-    name: 'dasddfssad',
+    "id": "k5rck4Q08gHvtRncXhJE",
+    "iconUrl": "icon",
+    "name": "Baby Needs",
+    "imageUrl": "https://res.cloudinary.com/giftforgood/image/upload/v1588842133/babyneeds.jpg"
   },
+  {
+    "name": "Kitchenware",
+    "iconUrl": "N.A.",
+    "imageUrl": "https://res.cloudinary.com/giftforgood/image/upload/v1588842219/kitchenware.jpg",
+    "id": "l61OrCHqvqagENbJr6BY"
+  },
+  {
+    "iconUrl": "fake_url",
+    "name": "Furniture",
+    "imageUrl": "https://res.cloudinary.com/giftforgood/image/upload/v1588842161/furniture.jpg",
+    "id": "svFwKXrxv0KFkPc5oh95"
+  }
 ];
 
 const Categories = () => {
-  const [categoriesObj, setCategoriesObj] = useState({});
-  useEffect(() => {
-    // setCategoriesObj(api.categories.getAll());
-  });
-
+  const [categories, setCategories] = useState([]);
+  // when testing, remove this
+  // useEffect(() => {
+  //   api.categories
+  //     .getAll()
+  //     .then((response) => {
+  //       const data = [];
+  //       response.docs.forEach((doc) => {
+  //         data.push(doc.data());
+  //       });
+  //       setCategories(data);
+  //       console.log(data);
+  //     })
+  //     .catch((err) => {})
+  // }, []);
+  
   const RowOfCategories = () => {
     const router = useRouter();
     return (
       <Stack direction="row" align="center">
+        {/* when testing, use dummyCategoriesData instead categories */}
         {dummyCategoriesData.map((category) => {
-          const href = '/category/' + category.name;
+          const href = `/category/${category.id}`;
           const handleClick = (event) => {
             event.preventDefault();
             router.push(href);
--- src/components/ImageHolder/SquareImageBox.js
+++ src/components/ImageHolder/SquareImageBox.js
@@ -1,41 +1,35 @@
 import { Text } from '@kiwicom/orbit-components/lib';
 import styled from 'styled-components';
 
-
 const SquareBox = styled.div`
   width: calc(75px + 2vw);
   height: calc(75px + 2vw);
   min-width: 75px;
   min-height: 75px;
   position: relative;
+  background: linear-gradient(rgba(0,0,0,0.5), rgba(0,0,0,0.5)), url(${props => props.src});
+  background-size: cover;
+  border-radius: 1vw;
 `;
 
 const CaptionInBox = styled.div`
   position: absolute;
-  top: 80%;
-  left: 10%;
+  bottom: 5%;
+  left: 5%;
   overflow-x: hidden;
 `;
 
-const styles = {
-  imageStyle: {
-    width: '100%',
-    height: '100%',
-    objectFit: 'cover',
-    filter: 'brightness(70%)',
-    borderRadius: '1vw',
-  },
-};
-
+/**
+ * 
+ * @param {string} imageUrl: the url of the image 
+ * @param {string} caption: the text displayed within the image 
+ * @param {string} captionSize: small, normal, large 
+ * @param {string} captionType: primary, secondary, info, success, warning, critical, white 
+ */
 const SquareImageBox = ({ imageUrl, caption, captionSize, captionType }) => {
   return (
-    <SquareBox>
-      <img style={styles.imageStyle} src={imageUrl} />
+    <SquareBox src={imageUrl}>
       <CaptionInBox>
-        {/* 
-          captionType: primary, secondary, info, success, warning, critical, white
-          captionSize: small, normal, large
-        */}
         <Text type={captionType || 'white'} size={captionSize || 'small'}>
           {caption}
         </Text>
