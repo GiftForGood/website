5ad291dc559eb8c6ce33698283551fa8ef5a111d
DanKhoo <danielkhoo94@hotmail.com>
2020-05-14 21:45:22 +0800

    create a chat page to test calendar modal

---
+++ pages/chat.js
@@ -0,0 +1,20 @@
+import React, { useState } from 'react';
+import { Button, Text } from '@kiwicom/orbit-components/lib';
+import CalendarModal from '../src/components/chat/modules/CalendarModal';
+
+const ChatPage = () => {
+  const [showModal, setShowModal] = useState(false);
+
+  const handleShowModal = () => setShowModal(true);
+  const handleCloseModel = () => setShowModal(false);
+
+  return (
+    <div>
+      <Text as="p">Welcome to Chat Page!</Text>
+      <Button onClick={handleShowModal}>Suggest Dates</Button>
+      <CalendarModal onShow={showModal} onHide={handleCloseModel} />
+    </div>
+  );
+};
+
+export default ChatPage;
