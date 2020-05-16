fa057a422e0e77c09471ddb3ca5762ab81ae4599
Marcus Koh <kohchihao@gmail.com>
2020-05-16 23:56:00 +0800

    Add examples

--- pages/index.js
+++ pages/index.js
@@ -3,6 +3,8 @@ import SessionProvider from '../src/components/session/modules/SessionProvider';
 import useUser from '../src/components/session/modules/useUser';
 import { isAuthenticated } from '../utils/authentication';
 import { withRedux } from '../utils/withRedux';
+import Verified from '../src/components/session/modules/Verified';
+
 export async function getServerSideProps({ params, req, res, query }) {
   let user = await isAuthenticated(req, res);
   return {
@@ -17,10 +19,12 @@ const HomePage = ({ user }) => {
 
   return (
     <SessionProvider user={user}>
-      <div>Welcome to Next.js Home page!!! {userData ? userData.name : ""}</div>
+      <div>Welcome to Next.js Home page!!! {userData ? userData.name : ''}</div>
+      <Verified>
+        {({ isDisabled }) => <button disabled={isDisabled}>Only Verified user can click on me</button>}
+      </Verified>
     </SessionProvider>
-    
-  )
-}
+  );
+};
 
 export default withRedux(HomePage);
