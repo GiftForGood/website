44eb812eafb0b3d42c38118573e92326e29d81aa
Marcus Koh <kohchihao@gmail.com>
2020-05-18 16:16:50 +0800

    Add verification checks

---
+++ utils/authentication/verification.js
@@ -0,0 +1,33 @@
+/**
+ * Checks if a user is verified by email or by admin.
+ *  user: A user from isAuthenticated
+ *  res: The response from getServerSideProps
+ *  destination: An object containing "Location" [Optional]
+ * 
+ * Eg: 
+ *   let isVerifiedUser = isVerified(user, res, { Location: '/register'});
+ *   let isVerifiedUser = isVerified(user, res);
+ */
+export function isVerified(user, res, destination = {}) {
+  try {
+    // NPO
+    if (user.npo && user.emailVerified && user.isVerifiedByAdmin) {
+      return true;
+    }
+
+    // Donor
+    if (user.donor && user.emailVerified) {
+      return true;
+    }
+
+    // No Donor/NPO
+    if (!destination.hasOwnProperty('Location')) {
+      return false;
+    }
+    throw new Error('Not Verified');
+  } catch (error) {
+    res.writeHead(302, destination);
+    res.end();
+    return null;
+  }
+}
