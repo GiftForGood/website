7ab63d3e9af093cd2f724828d4e429eba680eb13
Marcus Koh <kohchihao@gmail.com>
2020-05-19 13:50:54 +0800

    Run prettier

--- utils/constants/siteUrl.js
+++ utils/constants/siteUrl.js
@@ -1,12 +1,12 @@
-let BASE_URL = "";
-let FIREBASE_EMAIL_ACTION_URL = "";
+let BASE_URL = '';
+let FIREBASE_EMAIL_ACTION_URL = '';
 
-if (process.env.NODE_ENV === "development") {
-  BASE_URL = "http://localhost:3000";
-  FIREBASE_EMAIL_ACTION_URL = "http://localhost:3000/";
+if (process.env.NODE_ENV === 'development') {
+  BASE_URL = 'http://localhost:3000';
+  FIREBASE_EMAIL_ACTION_URL = 'http://localhost:3000/';
 } else {
-  BASE_URL = "https://giftforgood.io";
-  FIREBASE_EMAIL_ACTION_URL = "https://giftforgood.io";
+  BASE_URL = 'https://giftforgood.io';
+  FIREBASE_EMAIL_ACTION_URL = 'https://giftforgood.io';
 }
 
-export { BASE_URL, FIREBASE_EMAIL_ACTION_URL }
+export { BASE_URL, FIREBASE_EMAIL_ACTION_URL };
