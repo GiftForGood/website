77f111efa96faec6b8dfcb037f60b55e0262ea2b
jamessspanggg <jamespangmunwai@gmail.com>
2020-05-19 17:58:44 +0800

    Modify ui of search button

--- src/components/search/SearchBar.js
+++ src/components/search/SearchBar.js
@@ -2,6 +2,15 @@ import React from 'react';
 
 import Search from '@kiwicom/orbit-components/lib/icons/Search';
 import { InputField, Button } from '@kiwicom/orbit-components/lib';
+import styled from 'styled-components';
+
+const SearchButton = styled.button`
+  width: 20px;
+  height: 20px;
+  background: white;
+  margin: 10px;
+  padding: 0px;
+`;
 
 const SearchBar = () => {
   return (
@@ -9,7 +18,7 @@ const SearchBar = () => {
       inputMode="search"
       placeholder="Search post or users"
       suffix={
-        <Button type="white" onClick={function () {}}>
+      <Button type="white" asComponent={SearchButton} onClick={function () {}}>
           <Search />{' '}
         </Button>
       }
