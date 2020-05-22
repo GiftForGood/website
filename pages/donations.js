588f0b893f1d1a2b8e0a72ab8328b7461542c896
Marcus Koh <kohchihao@gmail.com>
2020-05-22 15:26:01 +0800

    Add nav bar to donation home apge

--- pages/donations.js
+++ pages/donations.js
@@ -1,12 +1,29 @@
 import React from 'react';
 import DonationsHomePage from '../src/components/home/pages/DonationsHomePage';
+import SessionProvider from '../src/components/session/modules/SessionProvider';
+import useUser from '../src/components/session/modules/useUser';
+import { isAuthenticated } from '../utils/authentication/authentication';
+import { withRedux } from '../utils/withRedux';
+import dynamic from 'next/dynamic';
+const TopNavigationBar = dynamic(() => import('../src/components/navbar/modules/TopNavigationBar'), { ssr: false });
 
-const DonationsHome = () => {
+export async function getServerSideProps({ params, req, res, query }) {
+  let user = await isAuthenticated(req, res);
+  return {
+    props: {
+      user,
+    },
+  };
+}
+
+const DonationsHome = ({ user }) => {
+  const userData = useUser();
   return (
-    <>
+    <SessionProvider user={user}>
+      <TopNavigationBar />
       <DonationsHomePage />
-    </>
+    </SessionProvider>
   );
 };
 
-export default DonationsHome;
+export default withRedux(DonationsHome);
