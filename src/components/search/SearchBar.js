54b21816f1f70fed939471782f1cb51f99a3c434
jamessspanggg <jamespangmunwai@gmail.com>
2020-05-24 21:37:57 +0800

    Remove redundant styled component

--- src/components/search/SearchBar.js
+++ src/components/search/SearchBar.js
@@ -2,15 +2,6 @@ import React from 'react';
 
 import Search from '@kiwicom/orbit-components/lib/icons/Search';
 import { InputField, ButtonLink } from '@kiwicom/orbit-components/lib';
-import styled from 'styled-components';
-
-const SearchButton = styled.button`
-  width: 80%;
-  height: 80%;
-  background: white;
-  margin-right: 5px;
-  padding: 0px;
-`;
 
 const SearchBar = () => {
   return (
