92851933d7139121466bdaed3f73c41ae7443d29
Marcus Koh <kohchihao@gmail.com>
2020-05-26 15:20:36 +0800

    Add empty files

---
+++ pages/settings/profile.js
@@ -0,0 +1,32 @@
+import React from 'react';
+import SessionProvider from '../../src/components/session/modules/SessionProvider';
+import { isAuthenticated } from '../../utils/authentication/authentication';
+import dynamic from 'next/dynamic';
+import NpoSettingProfilePage from '../../src/components/settings-profile/npo/pages/NpoSettingProfilePage';
+
+const TopNavigationBar = dynamic(() => import('../../src/components/navbar/modules/TopNavigationBar'), { ssr: false });
+
+
+export async function getServerSideProps({ params, req, res, query }) {
+  let user = await isAuthenticated(req, res);
+  if (!user) {
+    res.writeHead(302, { Location: '/' });
+    res.end();
+  }
+  return {
+    props: {
+      user,
+    },
+  };
+}
+
+const WishesHome = ({ user }) => {
+  return (
+    <SessionProvider user={user}>
+      <TopNavigationBar />
+      {user.user.npo ? <NpoSettingProfilePage /> : null}
+    </SessionProvider>
+  );
+};
+
+export default WishesHome;
