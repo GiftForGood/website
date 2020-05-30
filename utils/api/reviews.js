e2659468d28840830ce0335ba55670765118f8af
Jin Ying <kenneth.jyt@gmail.com>
2020-05-31 01:40:44 +0800

    Refractor

--- utils/api/reviews.js
+++ utils/api/reviews.js
@@ -1,4 +1,5 @@
 import { db } from '../firebase';
+import { REVIEWS_BATCH_SIZE } from './constants';
 import ReviewError from './error/reviewError';
 
 const reviewsCollection = db.collection('reviews');
@@ -16,19 +17,13 @@ class ReviewsAPI {
    * @return {object} A firebase document of the reviewed wish
    */
   async createWishReview(wishId, npoId, donorId, rating, review) {
-    const npoInfo = await this._getUserInfo('npos', npoId);
-    const donorInfo = await this._getUserInfo('donors', donorId);
-    const wishInfo = await this._getWishInfo(wishId);
+    this._validateRating(rating);
 
-    if (typeof npoInfo === 'undefined') {
-      throw new ReviewError('invalid-npo-id', 'npo does not exist');
-    }
-    if (typeof donorInfo === 'undefined') {
-      throw new ReviewError('invalid-donor-id', 'donor does not exist');
-    }
-    if (typeof wishInfo === 'undefined') {
-      throw new ReviewError('invalid-wish-id', 'wish does not exist');
-    }
+    const [npoInfo, donorInfo, wishInfo] = await Promise.all([
+      this._getUserInfo('npos', npoId),
+      this._getUserInfo('donors', donorId),
+      this._getWishInfo(wishId),
+    ]);
 
     if (wishInfo.status !== 'completed') {
       throw new ReviewError('invalid-wish-status', 'only can review a completed wish');
@@ -77,19 +72,13 @@ class ReviewsAPI {
    * @return {object} A firebase document of the reviewed donation
    */
   async createDonationReview(donationId, donorId, npoId, rating, review) {
-    const npoInfo = await this._getUserInfo('npos', npoId);
-    const donorInfo = await this._getUserInfo('donors', donorId);
-    const donationInfo = await this._getDonationInfo(donationId);
+    this._validateRating(rating);
 
-    if (typeof npoInfo === 'undefined') {
-      throw new ReviewError('invalid-npo-id', 'npo does not exist');
-    }
-    if (typeof donorInfo === 'undefined') {
-      throw new ReviewError('invalid-donor-id', 'donor does not exist');
-    }
-    if (typeof donationInfo === 'undefined') {
-      throw new ReviewError('invalid-donation-id', 'donation does not exist');
-    }
+    const [npoInfo, donorInfo, donationInfo] = await Promise.all([
+      this._getUserInfo('npos', npoId),
+      this._getUserInfo('donors', donorId),
+      this._getDonationInfo(donationId),
+    ]);
 
     if (donationInfo.status !== 'completed') {
       throw new ReviewError('invalid-donation-status', 'only can review a completed donation');
@@ -127,18 +116,39 @@ class ReviewsAPI {
   }
 
   async _getUserInfo(collectionName, id) {
-    let snapshot = await db.collection(collectionName).doc(id).get();
+    const snapshot = await db.collection(collectionName).doc(id).get();
+
+    if (!snapshot.exists) {
+      throw new ReviewError('invalid-user-id', 'user does not exist');
+    }
+
     return snapshot.data();
   }
 
   async _getWishInfo(id) {
-    let snapshot = await db.collection('wishes').doc(id).get();
+    const snapshot = await db.collection('wishes').doc(id).get();
+  
+    if (!snapshot.exists) {
+      throw new ReviewError('invalid-wish-id', 'wish does not exist');
+    }
+
     return snapshot.data();
   }
 
   async _getDonationInfo(id) {
-    let snapshot = await db.collection('donations').doc(id).get();
+    const snapshot = await db.collection('donations').doc(id).get();
+
+    if (!snapshot.exists) {
+      throw new ReviewError('invalid-donation-id', 'donation does not exist');
+    }
+
     return snapshot.data();
+  }
+
+  _validateRating(rating) {
+    if (rating < 1 || rating > 5) {
+      throw new ReviewError('invalid-rating', 'rating only can be between 1 to 5')
+    }
   }
 }
 
