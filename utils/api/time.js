3fd6ddf9fb29de4e31a0cb7cc209fbeb8be5cce6
jamessspanggg <jamespangmunwai@gmail.com>
2020-06-01 13:39:32 +0800

    run prettier

--- utils/api/time.js
+++ utils/api/time.js
@@ -1,24 +1,24 @@
 import moment from 'moment';
 
 moment.updateLocale('en', {
-  relativeTime : {
-      future: "in %s",
-      past:   "%s",
-      s  : 'just now',
-      ss : '%ds',
-      m:  "1m",
-      mm: "%dm",
-      h:  "1h",
-      hh: "%dh",
-      d:  "1d",
-      dd: "%dd",
-      w:  "1w",
-      ww: "%dw",
-      M:  "1mo",
-      MM: "%dmo",
-      y:  "1yr",
-      yy: "%dyr"
-  }
+  relativeTime: {
+    future: 'in %s',
+    past: '%s',
+    s: 'just now',
+    ss: '%ds',
+    m: '1m',
+    mm: '%dm',
+    h: '1h',
+    hh: '%dh',
+    d: '1d',
+    dd: '%dd',
+    w: '1w',
+    ww: '%dw',
+    M: '1mo',
+    MM: '%dmo',
+    y: '1yr',
+    yy: '%dyr',
+  },
 });
 
 export const getTimeDifferenceFromNow = (timeInMilliSeconds) => {
