91dcc2081b6e5de1d391a3c0d0792dbd99fcf07e
Marcus Koh <kohchihao@gmail.com>
2020-06-01 17:26:01 +0800

    Add comment

--- src/components/session/modules/Verified.js
+++ src/components/session/modules/Verified.js
@@ -3,6 +3,7 @@ import useUser from '../../../components/session/modules/useUser';
 const Verified = (props) => {
   const user = useUser();
 
+  // user is initially null --> populated with user info. null should disable children.
   if (user === null) {
     return props.children({
       isDisabled: true,
