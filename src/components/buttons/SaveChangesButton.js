ec142efa9010ad999b066778c9ea5acb957b764e
Marcus Koh <kohchihao@gmail.com>
2020-06-04 13:47:37 +0800

    Edit profile for NPO (#121)
    * Update profile setting url
    
    * Fix silent login
    
    * Add empty files
    
    * Add basic edit profile page
    
    * Use inputfile instead of button
    
    * Update profile
    
    * Update package-lock.json

---
+++ src/components/buttons/SaveChangesButton.js
@@ -0,0 +1,20 @@
+import styled from 'styled-components';
+import { colors } from '../../../utils/constants/colors';
+
+const SaveChangesButton = styled.button`
+  background: ${colors.saveChangesBackground};
+
+  :active {
+    background: ${colors.saveChangesHoverActive};
+  }
+
+  :hover {
+    background: ${colors.saveChangesHoverActive};
+  }
+
+  :focus {
+    box-shadow: 0 0 0 3px rgba(46, 46, 46, 0.5);
+  }
+`;
+
+export default SaveChangesButton;
