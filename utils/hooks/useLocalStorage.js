e21a1a3910513511f7e509bf1b49eb486b8c294d
Marcus Koh <kohchihao@gmail.com>
2020-06-06 10:25:26 +0800

    Given credit where credit is due

--- utils/hooks/useLocalStorage.js
+++ utils/hooks/useLocalStorage.js
@@ -1,3 +1,5 @@
+// Taken from https://usehooks.com/useLocalStorage/
+
 import { useState } from 'react';
 
 function useLocalStorage(key, initialValue) {
@@ -11,7 +13,7 @@ function useLocalStorage(key, initialValue) {
       return item ? JSON.parse(item) : initialValue;
     } catch (error) {
       // If error also return initialValue
-      console.log(error);
+      console.error(error);
       return initialValue;
     }
   });
@@ -28,7 +30,7 @@ function useLocalStorage(key, initialValue) {
       window.localStorage.setItem(key, JSON.stringify(valueToStore));
     } catch (error) {
       // A more advanced implementation would handle the error case
-      console.log(error);
+      console.error(error);
     }
   };
 
