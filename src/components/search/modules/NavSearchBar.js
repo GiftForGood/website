7495e61a210c2290e68b04f789a1a5ae95ee691e
Marcus Koh <kohchihao@gmail.com>
2020-06-21 13:52:43 +0800

    Fix searchbar click issue

--- src/components/search/modules/NavSearchBar.js
+++ src/components/search/modules/NavSearchBar.js
@@ -1,10 +1,11 @@
-import React, { useRef } from 'react';
+import React, { useRef, useState } from 'react';
 import { InstantSearch, connectSearchBox, Index, Configure, connectHits } from 'react-instantsearch-dom';
 import SearchBar from '../SearchBar';
 import Hits from './Hits';
 import algoliasearch from 'algoliasearch/lite';
 import styled from 'styled-components';
 import { Text, Popover } from '@kiwicom/orbit-components/lib';
+import { useEffect } from 'react';
 
 const CustomSearchBox = connectSearchBox(SearchBar);
 const CustomHits = connectHits(Hits);
@@ -16,18 +17,30 @@ const Container = styled.div`
 
 const NavSearchBar = () => {
   const inputRef = useRef(null);
+  const [isOpen, setIsOpen] = useState(false);
 
   const popoverOnOpen = () => {
-    setTimeout(() => {
-      inputRef.current.focus();
-    }, 150);
+    setIsOpen(true);
   };
+
+  const popoverOnClose = () => {
+    setIsOpen(false);
+  };
+
+  useEffect(() => {
+    if (isOpen) {
+      setTimeout(() => {
+        inputRef.current.focus();
+      }, 150);
+    }
+  }, [isOpen]);
 
   return (
     <InstantSearch searchClient={searchClient} indexName="wishes">
       <Popover
         width="350px"
         onOpen={popoverOnOpen}
+        onClose={popoverOnClose}
         content={
           <>
             <Index indexName="wishes">
