29d2c74a18303fa465c659bdbbdbbeae77e2821c
Marcus Koh <kohchihao@gmail.com>
2020-06-24 23:08:08 +0800

    prefetch home pages

--- src/components/navbar/modules/TopLeftNavigation.js
+++ src/components/navbar/modules/TopLeftNavigation.js
@@ -3,9 +3,10 @@ import { Stack, ButtonLink } from '@kiwicom/orbit-components/';
 import { MenuHamburger } from '@kiwicom/orbit-components/lib/icons';
 import useMediaQuery from '@kiwicom/orbit-components/lib/hooks/useMediaQuery';
 import LogoButton from '../../buttons/LogoButton';
-import SearchBar from '../../search/SearchBar';
 import { companyLogoImagePath } from '../../../../utils/constants/imagePaths';
 import styled from 'styled-components';
+import NavSearchBar from '../../search/modules/NavSearchBar';
+import Link from 'next/link';
 
 const SearchBarContainer = styled.div`
   min-width: 350px;
@@ -20,15 +21,20 @@ const TopLeftNavigation = ({ onHamburgerClick }) => {
           <LogoButton src={companyLogoImagePath} height={45} href={'/'} />
           {isDesktop && (
             <SearchBarContainer>
-              <SearchBar />
+              <NavSearchBar />
             </SearchBarContainer>
           )}
-          <ButtonLink transparent type="secondary" href={'/'}>
-            Wishes
-          </ButtonLink>
-          <ButtonLink transparent type="secondary" href={'/donations'}>
-            Donations
-          </ButtonLink>
+          <Link href="/">
+            <ButtonLink transparent type="secondary" href={'/'}>
+              Wishes
+            </ButtonLink>
+          </Link>
+
+          <Link href="/donations">
+            <ButtonLink transparent type="secondary" href={'/donations'}>
+              Donations
+            </ButtonLink>
+          </Link>
         </>
       ) : (
         <ButtonLink iconLeft={<MenuHamburger />} transparent type="secondary" onClick={onHamburgerClick} />
