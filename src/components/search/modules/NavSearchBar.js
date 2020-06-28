cd75271ed3abfd9ad5bb3d3ff461d36c127a791b
Marcus Koh <kohchihao@gmail.com>
2020-06-28 14:22:14 +0800

    Use a new popover library to handle search bar

--- src/components/search/modules/NavSearchBar.js
+++ src/components/search/modules/NavSearchBar.js
@@ -4,9 +4,9 @@ import SearchBar from '../SearchBar';
 import Hits from './Hits';
 import algoliasearch from 'algoliasearch/lite';
 import styled from 'styled-components';
-import { Text, Popover } from '@kiwicom/orbit-components/lib';
-import { useEffect } from 'react';
+import { Text } from '@kiwicom/orbit-components/lib';
 import { MAXIMUM_SEARCH } from '../../../../utils/constants/search';
+import Popover from 'react-tiny-popover';
 
 const CustomSearchBox = connectSearchBox(SearchBar);
 const CustomHits = connectHits(Hits);
@@ -16,55 +16,63 @@ const Container = styled.div`
   padding-top: 10px;
 `;
 
+const ContentContainer = styled.div`
+  background: white;
+  padding: 0px 10px 8px 10px;
+`;
+
 const NavSearchBar = () => {
   const inputRef = useRef(null);
   const [isOpen, setIsOpen] = useState(false);
 
-  const popoverOnOpen = () => {
-    setIsOpen(true);
-  };
-
-  const popoverOnClose = () => {
-    setIsOpen(false);
-  };
-
-  useEffect(() => {
-    if (isOpen) {
-      setTimeout(() => {
-        inputRef.current.focus();
-      }, 150);
-    }
-  }, [isOpen]);
-
   return (
     <InstantSearch searchClient={searchClient} indexName="wishes">
       <Popover
-        width="350px"
-        onOpen={popoverOnOpen}
-        onClose={popoverOnClose}
-        content={
-          <>
-            <Index indexName="wishes">
-              <Container>
-                <Text weight="bold">Wishes</Text>
-              </Container>
 
-              <CustomHits type="wishes" />
-              <Configure hitsPerPage={MAXIMUM_SEARCH} />
-            </Index>
+        isOpen={isOpen}
+        position={['bottom']} 
+        disableReposition 
+        onClickOutside={() => setIsOpen(false)}
+        containerStyle={{
+          zIndex: 1000,
+          width: '350px',
+          boxShadow: '0 8px 13px 0 rgba(44, 44, 45, 0.27)',
+          borderRadius: '5px',
+          position: 'fixed'
+        }}
+        align="start"
+        transitionDuration={0.1}
+        content={(
+          { position, nudgedLeft, nudgedTop, targetRect, popoverRect } 
+        ) => {
+          return (
+            <ContentContainer>
+              <>
+                <Index indexName="wishes">
+                  <Container>
+                    <Text weight="bold">Wishes</Text>
+                  </Container>
 
-            <Index indexName="donations">
-              <Container>
-                <Text weight="bold">Donations</Text>
-              </Container>
+                  <CustomHits type="wishes" />
+                  <Configure hitsPerPage={MAXIMUM_SEARCH} />
+                </Index>
 
-              <CustomHits type="donations" />
-              <Configure hitsPerPage={MAXIMUM_SEARCH} />
-            </Index>
-          </>
-        }
+                <Index indexName="donations">
+                  <Container>
+                    <Text weight="bold">Donations</Text>
+                  </Container>
+
+                  <CustomHits type="donations" />
+                  <Configure hitsPerPage={MAXIMUM_SEARCH} />
+                </Index>
+              </>
+            </ContentContainer>
+          );
+        }}
       >
-        <CustomSearchBox inputRef={inputRef} />
+        <div onClick={() => setIsOpen(true)}>
+          <CustomSearchBox inputRef={inputRef} />
+        </div>
       </Popover>
     </InstantSearch>
   );
