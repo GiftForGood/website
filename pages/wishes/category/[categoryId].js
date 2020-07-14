10a823bad50b02408bc171ec822a58c3d4a118a6
Marcus Koh <kohchihao@gmail.com>
2020-07-14 22:23:59 +0800

    Remove unused code

--- pages/wishes/category/[categoryId].js
+++ pages/wishes/category/[categoryId].js
@@ -1,7 +1,6 @@
 import React from 'react';
 import api from '../../../utils/api';
-//import ViewCategoryPage from '../../../src/components/category/pages/WishesViewCategoryPage';
-import ViewCategoryPage from '../../../src/components/category/pages/WishesViewCategoryPagea';
+import ViewCategoryPage from '../../../src/components/category/pages/WishesViewCategoryPage';
 
 import dynamic from 'next/dynamic';
 import SessionProvider from '../../../src/components/session/modules/SessionProvider';
@@ -19,7 +18,7 @@ export async function getServerSideProps({ params, query, req, res }) {
   return {
     props: {
       categoryDetails,
-      filterQuery: query.filter ? query.filter : null,
+      sortByQuery: query.sortBy ? query.sortBy : null,
       user,
     },
   };
@@ -33,14 +32,14 @@ const getCategoryDetails = async (categoryId) => {
   return rawCategory.data();
 };
 
-const ViewCategory = ({ categoryDetails, filterQuery, user }) => {
+const ViewCategory = ({ categoryDetails, sortByQuery, user }) => {
   if (Object.keys(categoryDetails).length === 0) {
     return <Error />;
   }
   return (
     <SessionProvider user={user}>
       <TopNavigationBar />
-      <ViewCategoryPage categoryDetails={categoryDetails} filterQuery={filterQuery} />
+      <ViewCategoryPage categoryDetails={categoryDetails} sortByQuery={sortByQuery} />
       <BottomNavigation />
     </SessionProvider>
   );
--- src/components/category/modules/WishesSortBy.js
+++ src/components/category/modules/WishesSortBy.js
@@ -1,6 +1,7 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import { ChoiceGroup, Radio, Separator } from '@kiwicom/orbit-components/lib';
 import BlackText from '../../text/BlackText';
+import { useRouter } from 'next/router';
 
 /**
  * https://www.algolia.com/doc/api-reference/widgets/sort-by/react/#create-a-react-component
@@ -8,11 +9,29 @@ import BlackText from '../../text/BlackText';
  * @param {string} currentRefinement
  * @param {function} refine
  */
-const WishesSortBy = ({ items, currentRefinement, refine }) => {
+const WishesSortBy = ({ items, currentRefinement, refine, sortByIndex, category }) => {
+  const router = useRouter();
+
   const handleSelect = (event) => {
     const sortBy = event.target.value;
     refine(sortBy);
+    if (category) {
+      router.push(`/wishes/category/[categoryId]`, `/wishes/category/${category.id}?sortBy=${event.target.value}`, {
+        shallow: true,
+      });
+    } else {
+      // for view all wishes
+      router.push(`/wishes/category`, `/wishes/category?sortBy=${event.target.value}`, {
+        shallow: true,
+      });
+    }
   };
+
+  useEffect(() => {
+    if (sortByIndex) {
+      refine(sortByIndex)
+    }
+  }, [sortByIndex, refine])
 
   return (
     <>
