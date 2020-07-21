7f3bb1390e222b8ad4eab5a54b6da03450aad342
jamessspanggg <jamespangmunwai@gmail.com>
2020-07-21 16:58:00 +0800

    Overwrite and use custom react-infinite-scroller

--- src/components/chat/modules/ChatDialogMessages.js
+++ src/components/chat/modules/ChatDialogMessages.js
@@ -5,7 +5,7 @@ import styled from 'styled-components';
 import { CardSection } from '@kiwicom/orbit-components/lib/Card';
 import useMediaQuery from '@kiwicom/orbit-components/lib/hooks/useMediaQuery';
 import NewChatTips from './NewChatTipsForWish';
-import InfiniteScroll from 'react-infinite-scroller';
+import InfiniteScroll from '../../scroller/InfiniteScroller';
 import { wishes } from '../../../../utils/constants/postType';
 import { CHAT_MESSAGES_BATCH_SIZE } from '../../../../utils/api/constants';
 import { LeftMessageSection, RightMessageSection } from './ChatMessageSection';
