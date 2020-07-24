94fc01237606a6eece9530759467c30579dda28f
James Pang <jamespangmunwai@gmail.com>
2020-07-24 16:16:21 +0800

    Make height of chat page exactly fit into mobile (#205)
    * Make height of chat page exactly fit into mobile
    
    * Switch to use arrow functions
    
    Co-authored-by: Marcus Koh <kohchihao@gmail.com>

--- src/components/chat/modules/ChatDialogMessages.js
+++ src/components/chat/modules/ChatDialogMessages.js
@@ -10,6 +10,7 @@ import { wishes } from '../../../../utils/constants/postType';
 import { CHAT_MESSAGES_BATCH_SIZE } from '../../../../utils/api/constants';
 import { LeftMessageSection, RightMessageSection } from './ChatMessageSection';
 import useStayScrolled from 'react-stay-scrolled';
+import useWindowDimensions from '../../../../utils/hooks/useWindowDimensions';
 
 /**
  * To be changed if any of the heights change, the extra "+1 or +2" for the top/bottom borders
@@ -23,15 +24,15 @@ const desktopHeights = {
 
 const mobileHeights = {
   chatDialogBackButton: 44,
-  chatDialogUserRow: 104 + 1,
+  chatDialogUserRow: 108 + 1,
   chatDialogSeePostRow: 96 + 1,
   chatDialogMessagesPadding: 32 + 2,
 };
 
 const MessageContainer = styled.div`
   width: 100%;
-  min-height: calc(100vh - ${({ offsetHeight }) => offsetHeight}px);
-  max-height: calc(100vh - ${({ offsetHeight }) => offsetHeight}px);
+  min-height: ${({ offsetHeight, viewportHeight }) => viewportHeight - offsetHeight}px;
+  max-height: ${({ offsetHeight, viewportHeight }) => viewportHeight - offsetHeight}px;
   position: relative;
   overflow-y: scroll;
   overflow-x: hidden;
@@ -51,6 +52,7 @@ const ChatDialogMessages = ({ postType, loggedInUser, selectedChatId, isNewChat,
   const { isTablet } = useMediaQuery();
   const scrollerRef = useRef(null);
   const { stayScrolled, scrollBottom } = useStayScrolled(scrollerRef);
+  const { height: viewportHeight } = useWindowDimensions();
 
   useEffect(() => {
     // reset values every time selectedChatId changes
@@ -139,7 +141,7 @@ const ChatDialogMessages = ({ postType, loggedInUser, selectedChatId, isNewChat,
   if (isNewChat && !selectedChatId && postType === wishes) {
     return (
       <CardSection>
-        <MessageContainer offsetHeight={offsetHeight} ref={scrollerRef}>
+        <MessageContainer offsetHeight={offsetHeight} ref={scrollerRef} viewportHeight={viewportHeight}>
           <NewChatTips postType={postType} />
         </MessageContainer>
       </CardSection>
@@ -148,7 +150,7 @@ const ChatDialogMessages = ({ postType, loggedInUser, selectedChatId, isNewChat,
 
   return (
     <CardSection>
-      <MessageContainer offsetHeight={offsetHeight} ref={scrollerRef}>
+      <MessageContainer offsetHeight={offsetHeight} ref={scrollerRef} viewportHeight={viewportHeight}>
         <InfiniteScroll
           loadMore={handleOnSeeMore}
           hasMore={shouldSeeMore}
