5d17275b2babb67781412fef86a67afe227e1779
Jin Ying <kenneth.jyt@gmail.com>
2020-08-01 11:10:34 +0800

    AuthAPI: Add notification data to npos and donors

---
+++ utils/constants/notification.js
@@ -0,0 +1,14 @@
+export const NPO_NOTIFICATION = {
+  dailyEmailForChat: -1,
+  weeklyEmailForChat: {
+    day: 1,
+    hour: 9,
+  },
+};
+export const DONOR_NOTIFICATION = {
+  dailyEmailForChat: -1,
+  weeklyEmailForChat: {
+    day: 1,
+    hour: 9,
+  },
+};
