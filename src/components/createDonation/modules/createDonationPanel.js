ab821ef6a778db6017bb5afe25269e9bdc0b4014
Marcus Koh <kohchihao@gmail.com>
2020-08-03 16:47:30 +0800

    Run prettier

--- src/components/createDonation/modules/createDonationPanel.js
+++ src/components/createDonation/modules/createDonationPanel.js
@@ -510,13 +510,13 @@ const CreateDonationPanel = ({ mode, donation }) => {
                     flex={['0 0 60px', '1 1 100%', '0 0 90px']}
                     label="Valid From"
                     error={
-                      (formik.touched.validFromDay || formik.touched.validFromMonth || formik.touched.validFromYear)
-                        ? (formik.errors.validFromDay ||
+                      formik.touched.validFromDay || formik.touched.validFromMonth || formik.touched.validFromYear
+                        ? formik.errors.validFromDay ||
                           formik.errors.validFromMonth ||
                           formik.errors.validFromYear ||
                           formik.errors.customValidFromValidation ||
                           formik.errors.customValidFromValidationMoreThanZeroDayFromToday ||
-                          formik.errors.customValidateDateRange)
+                          formik.errors.customValidateDateRange
                         : ''
                     }
                     required
@@ -540,13 +540,13 @@ const CreateDonationPanel = ({ mode, donation }) => {
                     flex={['0 0 60px', '1 1 100%', '0 0 90px']}
                     label="To"
                     error={
-                      (formik.touched.validToDay || formik.touched.validToMonth || formik.touched.validToYear)
-                        ? (formik.errors.validToDay ||
+                      formik.touched.validToDay || formik.touched.validToMonth || formik.touched.validToYear
+                        ? formik.errors.validToDay ||
                           formik.errors.validToMonth ||
                           formik.errors.validToYear ||
                           formik.errors.customValidToValidation ||
                           formik.errors.customValidToValidationMoreThanZeroDayFromToday ||
-                          formik.errors.customValidateDateRange)
+                          formik.errors.customValidateDateRange
                         : ''
                     }
                     required
