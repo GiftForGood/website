27caaa3c6f0336e7996c6cafd5d450c3fcb67830
Marcus Koh <kohchihao@gmail.com>
2020-08-25 00:00:07 +0800

    Remove redux from create-donation page (#304)
    * Remove redux from create-donation page
    
    * Remove duplicated code

--- store.js
+++ store.js
@@ -5,7 +5,6 @@ import logger from 'redux-logger';
 import registerReducer from './src/components/register';
 import loginReducer from './src/components/login';
 import sessionReducer from './src/components/session';
-import createDonationReducer from './src/components/createDonation';
 import navbarReducer from './src/components/navbar';
 
 const initialState = {};
@@ -14,7 +13,6 @@ const rootReducer = combineReducers({
   register: registerReducer,
   login: loginReducer,
   session: sessionReducer,
-  createDonation: createDonationReducer,
   navbar: navbarReducer,
 });
 
