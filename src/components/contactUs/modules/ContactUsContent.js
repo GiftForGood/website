8293440f6e64e03083fcae85f9d634cfcba77810
jamessspanggg <jamespangmunwai@gmail.com>
2020-08-27 23:59:46 +0800

    Add first draft for contact us page

---
+++ src/components/contactUs/modules/ContactUsContent.js
@@ -0,0 +1,57 @@
+import React from 'react';
+import styled, { css } from 'styled-components';
+import media from '@kiwicom/orbit-components/lib/utils/mediaQuery';
+import { Heading, Text, TextLink } from '@kiwicom/orbit-components/lib';
+
+const TitleContainer = styled.div`
+  margin-bottom: 50px;
+`;
+
+const MainContainer = styled.div`
+  max-width: 1280px;
+  margin: 0 auto;
+  margin-top: 50px;
+  padding: 0 10px;
+`;
+
+const ContactUsContainer = styled.div`
+  margin-bottom: 50px;
+`;
+
+const FeedbackContainer = styled.div`
+  margin-bottom: 50px;
+`;
+
+const ContactUsContent = () => {
+  return (
+    <MainContainer>
+      <TitleContainer>
+        <Heading as="h1" type="display">
+          Get in touch with
+        </Heading>
+        <Heading as="h1" type="display">
+          GiftforGood
+        </Heading>
+      </TitleContainer>
+      <ContactUsContainer>
+        <Text size="large" weight="bold" spaceAfter="normal">
+          Contact us
+        </Text>
+        <TextLink size="large" href="mailto: support@giftforgood.io">
+          support@giftforgood.io
+        </TextLink>
+      </ContactUsContainer>
+      <FeedbackContainer>
+        <Text size="large" weight="bold" spaceAfter="normal">
+          Send us your feedback
+        </Text>
+        <Text size="large">
+          If you have a query, feedback or wish to report a problem related to GiftforGood, please fill in the{' '}
+          <TextLink href="mailto: support@giftforgood.io">online form</TextLink>
+        </Text>
+      </FeedbackContainer>
+    </MainContainer>
+  );
+};
+
+export default ContactUsContent;
---
+++ src/components/contactUs/pages/ContactUsPage.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import ContactUsContent from '../modules/ContactUsContent';
+
+const ContactUsPage = () => {
+  return <ContactUsContent />;
+};
+
+export default ContactUsPage;
