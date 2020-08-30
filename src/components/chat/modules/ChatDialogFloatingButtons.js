f3405b444eedc1ab7b8a7e4d3811bdf0dceac59c
jamessspanggg <jamespangmunwai@gmail.com>
2020-08-30 23:19:41 +0800

    Use context for floating buttons

--- src/components/chat/modules/ChatDialogFloatingButtons.js
+++ src/components/chat/modules/ChatDialogFloatingButtons.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useState, useContext } from 'react';
 import { Button, Stack } from '@kiwicom/orbit-components/lib';
 import CalendarModal from '../../calendar/modules/CalendarModal';
 import SuggestDateButton from '../../../components/buttons/ChatSuggestDatesButton';
@@ -7,18 +7,17 @@ import styled from 'styled-components';
 import { getFormattedDateRange } from '../../../../utils/api/time';
 import { donations } from '../../../../utils/constants/postType';
 import { useRouter } from 'next/router';
+import ChatContext from '../context';
+import { setSelectedChatId, setIsNewChat } from '../actions';
+import { getSelectedChatId, getIsNewChat } from '../selectors';
 
 const ButtonsContainer = styled.div`
   overflow-x: scroll;
 `;
-const ChatDialogFloatingButtons = ({
-  postType,
-  postId,
-  isNewChat,
-  setIsNewChat,
-  selectedChatId,
-  setSelectedChatId,
-}) => {
+const ChatDialogFloatingButtons = ({ postType, postId }) => {
+  const { state, dispatch } = useContext(ChatContext);
+  const isNewChat = getIsNewChat(state);
+  const selectedChatId = getSelectedChatId(state);
   const [showSuggestDateModal, setShowSuggestDateModal] = useState(false);
 
   const handleShowSuggestDateModal = () => setShowSuggestDateModal(true);
@@ -31,8 +30,8 @@ const ChatDialogFloatingButtons = ({
       postType === donations ? 'sendInitialCalendarMessageForDonation' : 'sendInitialCalendarMessageForWish';
     const [rawChat, rawFirstMessage] = await api.chats[method](postId, calendarRawString);
     const chatId = rawChat.data().chatId;
-    setIsNewChat(false);
-    setSelectedChatId(chatId);
+    dispatch(setIsNewChat(false));
+    dispatch(setSelectedChatId(chatId));
     router.push(`/chat`, `/chat?chatId=${chatId}`, { shallow: true });
   };
 
