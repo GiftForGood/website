f136d46702101ab43895c536f850cad1b3e39829
Marcus Koh <kohchihao@gmail.com>
2020-09-04 17:04:04 +0800

    Add missing env

--- utils/firebase/index.js
+++ utils/firebase/index.js
@@ -12,6 +12,7 @@ var config = {
   storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
   messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
   appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
+  measurementId: process.env.NEXT_PUBLIC_FIREBASE_MEASUREMENT_ID,
 };
 
 if (!firebase.apps.length) {
