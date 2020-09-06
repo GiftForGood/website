d1804776b86f9c10903068d3b8a04182ac2a2141
James Pang <jamespangmunwai@gmail.com>
2020-09-07 00:11:28 +0800

    About us page (#312)
    * Add about us page UI
    
    * extract members as constant
    
    * Separate teams and refactor code
    
    * Add member descriptions and banner descriptions

---
+++ utils/constants/members.js
@@ -0,0 +1,85 @@
+export const marketingTeamMembers = [
+  {
+    imageUrl: 'https://res.cloudinary.com/giftforgood/image/upload/v1599102302/members/sneha.png',
+    name: 'Sneha Menon',
+    position: 'Marketing Head',
+    description:
+      'Sneha is a third year Business undergraduate at NUS with a passion for digital marketing and content development.',
+  },
+  {
+    imageUrl: 'https://res.cloudinary.com/giftforgood/image/upload/v1599099758/members/yoojin.jpg',
+    name: 'Yoojin Seo',
+    position: 'Marketing Executive',
+    description:
+      'Yoojin is a penultimate undergraduate at NUS Business School, with specialization in Marketing and Business Analytics. She has a keen interest in tech, creatives and social initiatives.',
+  },
+  {
+    imageUrl: 'https://res.cloudinary.com/giftforgood/image/upload/v1599061253/members/nicole.jpg',
+    name: 'Nicole Chua',
+    position: 'Marketing Associate',
+    description:
+      "Nicole is an undergraduate from NUS Business School. When she's not studying, you can find her hunting for good food in Singapore, or training hard to become a spin instructor at Revolution Singapore!",
+  },
+];
+
+export const partnershipTeamMembers = [
+  {
+    imageUrl: 'https://res.cloudinary.com/giftforgood/image/upload/v1599053704/members/jingwen.jpg',
+    name: 'Wong Jingwen',
+    position: 'Business Head',
+    description:
+      'Jingwen is a recent graduate from NUS Accountancy and is currently working in the FMCG industry. She is a volunteer tutor for underprivileged children and hopes to bring value to the nonprofit scene!',
+  },
+  {
+    imageUrl: 'https://res.cloudinary.com/giftforgood/image/upload/v1599099755/members/qinliang.jpg',
+    name: 'Yeo Qin-Liang',
+    position: 'Business Head',
+    description:
+      'Qin-Liang loves bringing people together and hopes to channel this energy towards connecting NPOs and donors on GiftforGood. Work aside, he has found the perfect popiah combination. Ask him about it!',
+  },
+  {
+    imageUrl: 'https://res.cloudinary.com/giftforgood/image/upload/v1599146285/members/sylvia.jpg',
+    name: 'Sylvia Chong',
+    position: 'Partnerships Executive',
+    description:
+      'Sylvia is currently a Year 2 student in NUS Business School. Outside of school, she is passionate about giving back to the community, and meeting new people along the way!',
+  },
+  {
+    imageUrl: 'https://res.cloudinary.com/giftforgood/image/upload/v1599061239/members/kert.jpg',
+    name: 'Kerrthegaa D/O Gopalan',
+    position: 'Partnerships Executive',
+    description:
+      "Currently in her final year (Economics), Kerrthegaa enjoys addressing community needs and working with NPOs across Singapore to build a better society. Besides that, she's an avid traveller!",
+  },
+];
+
+export const techTeamMembers = [
+  {
+    imageUrl: 'https://res.cloudinary.com/giftforgood/image/upload/v1598364061/members/marcuskoh.jpg',
+    name: 'Marcus Koh',
+    position: 'Tech Head',
+    description:
+      'Marcus has developed and launched multiple products including a telegram bot called @sgmrt_bot which was published at vulcanpost.com and a mobile app called NUSBuses which was covered by LianHe ZaoBao. He is also passionate in technologies like React Native and ReactJS.',
+  },
+  {
+    imageUrl: 'https://res.cloudinary.com/giftforgood/image/upload/v1598363998/members/jinying.jpg',
+    name: 'Tan Jin Ying',
+    position: 'Tech Head',
+    description:
+      'Jin Ying is constantly looking for ways that he can use his technical skills to add value to the community. Beside tech, he also has interest in personal finance and personal growth, to become a little wiser each day.',
+  },
+  {
+    imageUrl: 'https://res.cloudinary.com/giftforgood/image/upload/v1598358630/members/jamespang.jpg',
+    name: 'James Pang',
+    position: 'Fullstack Software Engineer',
+    description:
+      "James is a final year Computer Science undergraduate from NUS, he's interested in all things related to tech and is passionate to utilise his skillset to help out with the communities in need. Aside from work related stuffs, he enjoys travelling, photography and videography to seize precious moments.",
+  },
+  {
+    imageUrl: 'https://res.cloudinary.com/giftforgood/image/upload/v1598364669/members/danielkhoo.jpg',
+    name: 'Daniel Khoo',
+    position: 'Fullstack Software Engineer',
+    description:
+      'Daniel has developed multiple products including a telegram bot, an android app, and a web platform - weddingwishes.live. He is also into fitness, investment, and technology - hoping that he could put his skillsets into good use.',
+  },
+];
