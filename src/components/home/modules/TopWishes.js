49936234c91737515ae9dcf7bf7d48c54217c681
Marcus Koh <kohchihao@gmail.com>
2020-09-08 21:39:39 +0800

    Fix undefined facets

--- src/components/home/modules/TopWishes.js
+++ src/components/home/modules/TopWishes.js
@@ -68,6 +68,9 @@ const TopWishes = ({ numberOfPosts, numberOfCategories }) => {
   useEffect(() => {
     getAllCategories().then((categories) => {
       getTopNCategoriesFromAlgoliaWithExpireDateTime('wishes').then(({ hits, facets }) => {
+        if (facets['categories.id'] === undefined || Object.keys(facets['categories.id']).length === 0) {
+          return;
+        }
         const sorted = sortObjectEntries(facets['categories.id']);
         if (sorted.length >= numberOfCategories) {
           const topNCategoriesIds = sorted.slice(0, numberOfCategories);
