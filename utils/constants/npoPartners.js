4a2e5049e11067c7478bc002fcd11327141cb781
Daniel Khoo <danielkhoo94@hotmail.com>
2020-09-13 20:57:15 +0800

    Add more npo partners (#352)

--- utils/constants/npoPartners.js
+++ utils/constants/npoPartners.js
@@ -1,5 +1,13 @@
 export const npoPartners = [
   {
+    name: 'ADA',
+    imageUrl: 'https://res.cloudinary.com/giftforgood/image/upload/v1599993325/partners/ADA-logo_cqol7g.png',
+  },
+  {
+    name: 'Bethel Community Service',
+    imageUrl: 'https://res.cloudinary.com/giftforgood/image/upload/v1599993435/partners/BCS_Logo_with_Text_t7yy6d.jpg',
+  },
+  {
     name: 'Care Corner (Tampines)',
     imageUrl: 'https://res.cloudinary.com/giftforgood/image/upload/v1599742417/partners/CareCornerLogo_ugzc9d.png',
   },
@@ -7,5 +15,37 @@ export const npoPartners = [
     name: `Children's Aid Society`,
     imageUrl:
       'https://res.cloudinary.com/giftforgood/image/upload/v1599755328/partners/CAS_IconLogo_RGB_Full_fplsy5.jpg',
+  },
+  {
+    name: 'CPAS',
+    imageUrl: 'https://res.cloudinary.com/giftforgood/image/upload/v1599994338/partners/CPAS-logo-2_apjihk.png',
+  },
+  {
+    name: 'Habitat for Humanity',
+    imageUrl:
+      'https://res.cloudinary.com/giftforgood/image/upload/v1599993816/partners/Habitat_for_humanity_logo_eadrgc.png',
+  },
+  {
+    name: 'Lion Befrienders',
+    imageUrl:
+      'https://res.cloudinary.com/giftforgood/image/upload/v1599993968/partners/Lion_Befrienders_logo_sidw9w.png',
+  },
+  {
+    name: 'Morning Star',
+    imageUrl:
+      'https://res.cloudinary.com/giftforgood/image/upload/v1599994009/partners/MorningStar_Logo_RGB_flkapw.png',
+  },
+  {
+    name: 'New Life',
+    imageUrl: 'https://res.cloudinary.com/giftforgood/image/upload/v1599994047/partners/Newlifelogo_wntvaf.png',
+  },
+  {
+    name: 'SUN DAC',
+    imageUrl: 'https://res.cloudinary.com/giftforgood/image/upload/v1599994085/partners/SUN-DAC_Logo_zvykri.png',
+  },
+  {
+    name: 'Viriya',
+    imageUrl:
+      'https://res.cloudinary.com/giftforgood/image/upload/v1599994198/partners/Viriya_Community_Svc_Logo_RGB_zoavtd.png',
   },
 ];
