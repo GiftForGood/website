9f2c9e4e9cd216a631afee816437efc53fcb4029
Marcus Koh <kohchihao@gmail.com>
2020-09-17 17:56:18 +0800

    Add routing carousel to donations home page

--- src/components/home/pages/DonationsHomePage.js
+++ src/components/home/pages/DonationsHomePage.js
@@ -6,6 +6,7 @@ import { Grid } from '@kiwicom/orbit-components/lib';
 import styled, { css } from 'styled-components';
 import media from '@kiwicom/orbit-components/lib/utils/mediaQuery';
 import { donationsHomePageDetails } from '@constants/homePageDetails';
+import RoutingCarousel from '@components/carousel/RoutingCarousel';
 
 const HomePageContainer = styled.div`
   display: flex;
@@ -56,15 +57,18 @@ const DonationsHomePage = () => {
     <HomePageContainer>
       <Grid
         style={styles.gridContainer}
-        rows="1fr 0.5fr auto"
+        rows="1fr 0.25fr 0.5fr auto"
         rowGap="25px"
         columns="1fr"
         desktop={{
-          rows: '1.5fr 1fr auto',
+          rows: '1.5fr 0.25fr 1fr auto',
           rowGap: '30px',
         }}
       >
         <Banner type={pageType} />
+        <CategoriesContainer>
+          <RoutingCarousel />
+        </CategoriesContainer>
         <CategoriesContainer>
           <ResponsiveTitle>{categoriesTitle}</ResponsiveTitle>
           <Categories type="donations" />
