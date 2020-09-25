09349b99c16dd95e4419bfa24d9547929408db8d
Marcus Koh <kohchihao@gmail.com>
2020-09-25 20:56:01 +0800

    Redesign filter/sort on mobile and tablet (#371)

--- src/components/category/modules/DonationsSortFilterPanel.js
+++ src/components/category/modules/DonationsSortFilterPanel.js
@@ -1,31 +1,55 @@
-import React from 'react';
-import { Stack, Collapse } from '@kiwicom/orbit-components/lib';
+import React, { useState } from 'react';
+import { Stack, Button, Modal } from '@kiwicom/orbit-components/lib';
 import DonationsSortBy from '../modules/DonationsSortBy';
 import DonationsFilterBy from '../modules/DonationsFilterBy';
-import useMediaQuery from '@kiwicom/orbit-components/lib/hooks/useMediaQuery';
 import { connectSortBy } from 'react-instantsearch-dom';
+import Desktop from '@kiwicom/orbit-components/lib/Desktop';
+import Mobile from '@kiwicom/orbit-components/lib/Mobile';
+import styled from 'styled-components';
+import { ModalFooter, ModalSection } from '@kiwicom/orbit-components/lib/Modal';
+
+const ModalContainer = styled.div`
+  display: ${({ hide }) => (hide ? 'none' : 'box')};
+`;
 
 const DonationsSort = connectSortBy(DonationsSortBy);
 
 const DonationsSortFilterPanel = ({ sortItems, sortDefaultRefinement, category, onLatLngUpdated }) => {
-  const { isDesktop } = useMediaQuery();
+  const [isModalOpen, setIsModalOpen] = useState(false);
 
   return (
-    <Stack>
-      {isDesktop ? (
-        <>
+    <>
+      <Desktop>
+        <Stack>
           <DonationsSort items={sortItems} defaultRefinement={sortDefaultRefinement} category={category} />
           <DonationsFilterBy onLatLngUpdated={onLatLngUpdated} />
-        </>
-      ) : (
-        <Collapse label="Filter/Sort Settings">
-          <Stack>
-            <DonationsSort items={sortItems} defaultRefinement={sortDefaultRefinement} category={category} />
-            <DonationsFilterBy onLatLngUpdated={onLatLngUpdated} />
-          </Stack>
-        </Collapse>
-      )}
-    </Stack>
+        </Stack>
+      </Desktop>
+
+      <Mobile>
+        <Stack>
+          <Button onClick={() => setIsModalOpen(true)} size="small" type="secondary" width={0}>
+            Filter/Sort Settings
+          </Button>
+        </Stack>
+
+        <ModalContainer hide={!isModalOpen}>
+          <Modal onClose={() => setIsModalOpen(false)} size="small">
+            <ModalSection>
+              <Stack>
+                <DonationsSort items={sortItems} defaultRefinement={sortDefaultRefinement} category={category} />
+                <DonationsFilterBy onLatLngUpdated={onLatLngUpdated} />
+              </Stack>
+            </ModalSection>
+            <ModalFooter>
+              <Button fullWidth onClick={() => setIsModalOpen(false)} type="secondary">
+                Close
+              </Button>
+            </ModalFooter>
+          </Modal>
+        </ModalContainer>
+      </Mobile>
+    </>
   );
 };
 
--- src/components/category/modules/WishesSortFilterPanel.js
+++ src/components/category/modules/WishesSortFilterPanel.js
@@ -1,31 +1,55 @@
-import React from 'react';
-import { Stack, Collapse } from '@kiwicom/orbit-components/lib';
+import React, { useState } from 'react';
+import { Stack, Button, Modal } from '@kiwicom/orbit-components/lib';
+import Desktop from '@kiwicom/orbit-components/lib/Desktop';
+import Mobile from '@kiwicom/orbit-components/lib/Mobile';
 import WishesSortBy from '../modules/WishesSortBy';
 import WishesFilterby from '../modules/WishesFilterBy';
-import useMediaQuery from '@kiwicom/orbit-components/lib/hooks/useMediaQuery';
 import { connectSortBy } from 'react-instantsearch-dom';
+import { ModalFooter, ModalSection } from '@kiwicom/orbit-components/lib/Modal';
+import styled from 'styled-components';
+
+const ModalContainer = styled.div`
+  display: ${({ hide }) => (hide ? 'none' : 'box')};
+`;
 
 const WishesSort = connectSortBy(WishesSortBy);
 
 const WishesSortFilterPanel = ({ sortItems, sortDefaultRefinement, category, onLatLngUpdated }) => {
-  const { isDesktop } = useMediaQuery();
+  const [isModalOpen, setIsModalOpen] = useState(false);
 
   return (
-    <Stack>
-      {isDesktop ? (
-        <>
+    <>
+      <Desktop>
+        <Stack>
           <WishesSort items={sortItems} defaultRefinement={sortDefaultRefinement} category={category} />
           <WishesFilterby onLatLngUpdated={onLatLngUpdated} />
-        </>
-      ) : (
-        <Collapse label="Filter/Sort Settings">
-          <Stack>
-            <WishesSort items={sortItems} defaultRefinement={sortDefaultRefinement} category={category} />
-            <WishesFilterby onLatLngUpdated={onLatLngUpdated} />
-          </Stack>
-        </Collapse>
-      )}
-    </Stack>
+        </Stack>
+      </Desktop>
+
+      <Mobile>
+        <Stack>
+          <Button onClick={() => setIsModalOpen(true)} size="small" type="secondary" width={0}>
+            Filter/Sort Settings
+          </Button>
+        </Stack>
+
+        <ModalContainer hide={!isModalOpen}>
+          <Modal onClose={() => setIsModalOpen(false)} size="small">
+            <ModalSection>
+              <Stack>
+                <WishesSort items={sortItems} defaultRefinement={sortDefaultRefinement} category={category} />
+                <WishesFilterby onLatLngUpdated={onLatLngUpdated} />
+              </Stack>
+            </ModalSection>
+            <ModalFooter>
+              <Button fullWidth onClick={() => setIsModalOpen(false)} type="secondary">
+                Close
+              </Button>
+            </ModalFooter>
+          </Modal>
+        </ModalContainer>
+      </Mobile>
+    </>
   );
 };
 
