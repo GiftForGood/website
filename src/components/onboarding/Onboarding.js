94d3d040d17f541b79c2bd4096986c8e4605b273
Marcus Koh <kohchihao@gmail.com>
2020-10-01 15:36:56 +0800

    Add onboarding ui

---
+++ src/components/onboarding/Onboarding.js
@@ -0,0 +1,117 @@
+import React, { useState } from 'react';
+import PropTypes from 'prop-types';
+import styled from 'styled-components';
+import { Modal, Text, Stack } from '@kiwicom/orbit-components/lib';
+import { ModalSection, ModalHeader } from '@kiwicom/orbit-components/lib/Modal';
+import { Carousel } from 'react-responsive-carousel';
+import Button from '@kiwicom/orbit-components/lib/Button';
+import ButtonLink from '@kiwicom/orbit-components/lib/ButtonLink';
+import { onboardingDonor, onboardingNpo } from '@constants/onboarding';
+import { DONOR } from '@constants/usersType';
+import { colors } from '@constants/colors';
+import BlueButton from '@components/buttons/BlueButton';
+import RedButton from '@components/buttons/RedButton';
+
+const Image = styled.img`
+  height: 200px;
+  width: 100%;
+  margin-bottom: 50px;
+`;
+
+const BannerContentContainer = styled.div`
+  margin-bottom: 50px;
+`;
+
+const Indicator = styled.li`
+  background: ${({ selected, type }) =>
+    selected ? (type === DONOR ? colors.donorBackground : colors.npoBackground) : '#bdbdbd'};
+  width: 10px;
+  height: 10px;
+  display: inline-block;
+  margin: 0 8px;
+  border-radius: 10px;
+`;
+
+const BannerContent = ({ src, description }) => {
+  return (
+    <BannerContentContainer>
+      <Image src={src} />
+      <Text align="center">{description}</Text>
+    </BannerContentContainer>
+  );
+};
+
+const Onboarding = ({ type, show = false, name = '' }) => {
+  const [shown, setShown] = useState(show);
+  const [currentSlide, setCurrentSlide] = useState(0);
+  const [onboardingContent, setOnboardingContent] = useState(type === DONOR ? onboardingDonor : onboardingNpo);
+
+  const updateCurrentSlide = (index) => {
+    if (currentSlide !== index) {
+      setCurrentSlide(index);
+    }
+  };
+
+  return (
+    <>
+      {shown ? (
+        <Modal onClose={() => setShown(false)} size="small">
+          <ModalHeader title={`Welcome, ${name}!`}></ModalHeader>
+          <ModalSection>
+            <Carousel
+              showThumbs={false}
+              showStatus={false}
+              autoPlay={false}
+              stopOnHover
+              showArrows={false}
+              selectedItem={currentSlide}
+              onChange={updateCurrentSlide}
+              renderIndicator={(onClickHandler, isSelected, index, label) => {
+                if (isSelected) {
+                  return <Indicator selected={isSelected} type={type} />;
+                }
+                return (
+                  <Indicator
+                    onClick={onClickHandler}
+                    onKeyDown={onClickHandler}
+                    value={index}
+                    key={index}
+                    role="button"
+                    tabIndex={0}
+                    title={`${label} ${index + 1}`}
+                    aria-label={`${label} ${index + 1}`}
+                    type={type}
+                  />
+                );
+              }}
+            >
+              {onboardingContent.map((content) => (
+                <BannerContent src={content.src} description={content.description} />
+              ))}
+            </Carousel>
+
+            <Stack direction="row" justify="between">
+              <ButtonLink onClick={() => setShown(false)} type="secondary">
+                Skip intro
+              </ButtonLink>
+              <Button
+                asComponent={type === DONOR ? RedButton : BlueButton}
+                onClick={() => setCurrentSlide(currentSlide + 1)}
+              >
+                Next
+              </Button>
+            </Stack>
+          </ModalSection>
+        </Modal>
+      ) : null}
+    </>
+  );
+};
+
+Onboarding.propTypes = {
+  type: PropTypes.string.isRequired,
+  show: PropTypes.bool.isRequired,
+  name: PropTypes.string.isRequired,
+};
+
+export default Onboarding;
---
+++ utils/constants/onboarding.js
@@ -0,0 +1,24 @@
+export const onboardingDonor = [
+  {
+    src: 'https://res.cloudinary.com/giftforgood/image/upload/v1601521851/onboarding/undraw_send_gift_7o4m_lat4mf.svg',
+    description: 'You can donate by posting your items and wait for NPOs to chat with you.',
+  },
+  {
+    src: 'https://res.cloudinary.com/giftforgood/image/upload/v1601521853/onboarding/undraw_chatting_2yvo_jzcdik.svg',
+    description: 'You can donate by chatting with wishes on the platform and see how you can help.',
+  },
+];
+
+export const onboardingNpo = [
+  {
+    src:
+      'https://res.cloudinary.com/giftforgood/image/upload/v1601533846/onboarding/undraw_publish_post_vowb_lhsz01.svg',
+    description:
+      'You can request for items by posting a wish, with a title, description and location of delivery. Donors will be able to view them in individual category page.',
+  },
+  {
+    src: 'https://res.cloudinary.com/giftforgood/image/upload/v1601533848/onboarding/undraw_chatting_2yvo_1_f78qcq.svg',
+    description:
+      'You can request for donations by chatting with donations on the platform and see how they can help you.',
+  },
+];
