4bc9aca96ed44dd017424bf105cd912b271e07f5
James Pang <jamespangmunwai@gmail.com>
2020-10-08 14:05:54 +0800

    Add duplicate wish feature (#375)

--- pages/wishes/create.js
+++ pages/wishes/create.js
@@ -2,10 +2,13 @@ import React from 'react';
 import { isAuthenticated } from '@utils/authentication/authentication';
 import { isVerified } from '@utils/authentication/verification';
 import { isNpo } from '@utils/authentication/userType';
+import api from '@api';
 import SessionProvider from '@components/session/modules/SessionProvider';
 import CreateWishPage from '@components/createWish/pages/createWishPage';
 import dynamic from 'next/dynamic';
 import Header from '@components/header';
+import { deserializeFirestoreTimestampToUnixTimestamp } from '@utils/firebase/deserializer';
+import Error from 'next/error';
 
 const TopNavigationBar = dynamic(() => import('@components/navbar/modules/TopNavigationBar'), { ssr: false });
 const Footer = dynamic(() => import('@components/footer/Footer'), { ssr: false });
@@ -18,19 +21,51 @@ export async function getServerSideProps({ params, req, res, query }) {
   }
   isVerified(user.user, res, { Location: '/' });
   isNpo(user.user, res, { Location: '/' });
+
+  // duplicate wish
+  const wishId = query.wishId ? query.wishId : null;
+  let wish = null;
+  if (wishId) {
+    const wishDoc = await api.wishes.get(wishId).catch((err) => console.error(err));
+
+    if (!wishDoc.exists) {
+      return {
+        props: {
+          hasError: true,
+        },
+      };
+    }
+
+    // duplicate wish, but not the post owner
+    if (wishDoc.data().user.userId !== user.user.userId) {
+      return {
+        props: {
+          hasError: true,
+        },
+      };
+    }
+
+    wish = wishDoc.data();
+    deserializeFirestoreTimestampToUnixTimestamp(wish);
+  }
+
   return {
     props: {
       user,
+      wish,
     },
   };
 }
 
-const CreateWishes = ({ user }) => {
+const CreateWishes = ({ user, wish, hasError }) => {
+  if (hasError) {
+    return <Error />;
+  }
   return (
     <SessionProvider user={user}>
       <Header title="Create Wishes | GiftForGood" />
       <TopNavigationBar showNews={true} />
-      <CreateWishPage mode="create" />
+      <CreateWishPage mode="create" wish={wish} />
       <Footer />
     </SessionProvider>
   );
