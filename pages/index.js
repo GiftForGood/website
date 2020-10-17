daba440345e2cc244c6f0f1eb301e73b241bbdfb
Marcus Koh <kohchihao@gmail.com>
2020-10-18 00:27:54 +0800

    Add onboarding ui (#373)
    * Add onboarding ui
    
    * add more content for onboarding
    
    * Add linkify for links
    
    * Run prettier
    
    * Shallow route to home page when close or skip onboarding
    
    * Update relevant register page to route to onboarding ui
    
    * Update promocode svg
    
    * Add done button after onboarding

--- pages/index.js
+++ pages/index.js
@@ -1,12 +1,12 @@
 import React from 'react';
 import SessionProvider from '@components/session/modules/SessionProvider';
-import useUser from '@components/session/modules/useUser';
 import { isAuthenticated } from '@utils/authentication/authentication';
-import Verified from '@components/session/modules/Verified';
 import WishesHomePage from '@components/home/pages/WishesHomePage';
 import dynamic from 'next/dynamic';
 import Header from '@components/header';
 import { WISHES } from '@constants/search';
+import Onboarding from '@components/onboarding/Onboarding';
+import { DONOR, NPO } from '@constants/usersType';
 
 const TopNavigationBar = dynamic(() => import('@components/navbar/modules/TopNavigationBar'), { ssr: false });
 const BottomNavigation = dynamic(() => import('@components/navbar/modules/BottomNavigation'), { ssr: false });
@@ -14,15 +14,16 @@ const Footer = dynamic(() => import('@components/footer/Footer'), { ssr: false }
 
 export async function getServerSideProps({ params, req, res, query }) {
   let user = await isAuthenticated(req, res);
+  const { next } = query;
   return {
     props: {
       user,
+      next: next || null,
     },
   };
 }
 
-const WishesHome = ({ user }) => {
-  const userData = useUser();
+const WishesHome = ({ user, next }) => {
   return (
     <SessionProvider user={user}>
       <Header title="Wishes | GiftForGood" />
@@ -30,6 +31,12 @@ const WishesHome = ({ user }) => {
       <WishesHomePage />
       <BottomNavigation />
       <Footer />
+
+      <Onboarding
+        show={user && next === 'onboarding' ? true : false}
+        type={user?.user?.donor ? DONOR : NPO}
+        name={user?.user?.name}
+      />
     </SessionProvider>
   );
 };
