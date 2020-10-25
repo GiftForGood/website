b5da0e333fbd29a3482ba51c1d704a3ed85d2b63
DanKhoo <danielkhoo94@hotmail.com>
2020-10-25 20:21:19 +0800

    Update nvpc description

--- utils/constants/partners.js
+++ utils/constants/partners.js
@@ -4,6 +4,6 @@ export const partners = [
     imageUrl:
       'https://res.cloudinary.com/giftforgood/image/upload/v1598533391/footer/NVPC_Vertical_Logo_Colour_shifgv_lpe3t1.png',
     siteUrl: 'https://www.cityofgood.sg/nvpc.html',
-    description: `The National Volunteer & Philanthropy Centre (NVPC) is an independent not-for-profit organisation that advocates giving in Singapore. Their vision is for Singapore to be a Giving Nation and to cultivate a strong culture of contribution where giving is part of every Singaporean's DNA.  NVPC provides strategic advice and assistance in supporting the sustainable growth of GiftforGood.`,
+    description: `The National Volunteer & Philanthropy Centre (NVPC) is the steward of the City of Good vision for Singapore, where individuals, organisations, and leaders come together to give their best for others. Through our brands, programmes, and initiatives, we facilitate partnerships with non-profits, organisations, public sector bodies, and individuals to enliven the giving ecosystem within Singapore. We invite you to join us in building a #CityofGood together. `,
   },
 ];
