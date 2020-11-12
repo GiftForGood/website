9a4bf475b568d7ad05379bfbdc799bd01071d600
Marcus Koh <kohchihao@gmail.com>
2020-11-12 10:49:55 +0800

    Abstract out seasonal css

--- src/components/card/WishCard.js
+++ src/components/card/WishCard.js
@@ -5,6 +5,7 @@ import { getTimeDifferenceFromNow } from '@api/time';
 import styled, { css } from 'styled-components';
 import { useRouter } from 'next/router';
 import media from '@kiwicom/orbit-components/lib/utils/mediaQuery';
+import { SeasonalContainer, SeasonalSmallTag } from '@components/seasonal';
 
 const CardContainer = styled.div`
   display: flex;
@@ -81,24 +82,6 @@ const ClickableDiv = styled.a`
 
 const BadgeWrapper = styled.div`
   margin-bottom: 8px !important;
-`;
-
-const SeasonalContainer = styled.div`
-  position: absolute;
-  left: 50%;
-  top: -20px;
-  transform: translateX(-50%);
-  border-radius: 50%;
-  background-color: white;
-  height: 24px;
-  width: 24px;
-  padding: 10px;
-  box-shadow: 0px 0px 5px 0px rgba(37, 42, 49, 0.16), 0px 2px 8px 0px rgba(37, 42, 49, 0.12);
-`;
-
-const SeasonalSmallTag = styled.img`
-  width: 100%;
-  height: 100%;
 `;
 
 const CardDescription = ({ title, description }) => {
