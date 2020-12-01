792740fc23457a0b1fbf8ca4f5ef9bc32a4ce2a4
Marcus Koh <kohchihao@gmail.com>
2020-12-01 20:52:33 +0800

    Refactor container code

---
+++ src/components/containers/index.js
@@ -0,0 +1,9 @@
+import styled from 'styled-components';
+
+export const MaxWidthContainer = styled.div`
+  width: 90vw;
+  max-width: 1280px;
+  margin: 0 auto;
+  margin-top: ${props => props.marginTop ? props.marginTop : '50'}px;
+  margin-bottom: ${props => props.marginBottom ? props.marginBottom : '40'}px;
+`;
