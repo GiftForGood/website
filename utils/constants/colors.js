f87c030b38567de6557d779dd7eb492e08097716
Marcus Koh <kohchihao@gmail.com>
2020-12-01 14:59:57 +0800

    Update colors.js

--- utils/constants/colors.js
+++ utils/constants/colors.js
@@ -2,49 +2,133 @@ export const colors = {
   donorBackground: '#ed5555',
   donorHoverActive: '#eb3e3e',
   donorFocus: '#de1818',
+  primaryRed: {
+    background: '#ed5555',
+    hoverActive: '#eb3e3e',
+    focus: '#de1818'
+  },
   npoBackground: '#065ef5',
   npoHoverActive: '#0554dc',
   npoFocus: '#0441aa',
+  primaryBlue: {
+    background: "#065ef5",
+    hoverActive: "#0554dc",
+    focus: '#0441aa'
+  },
   calendarSelectedBackground: '#4db6ac',
   calendarSelectedHoverActive: '#35ada0',
   calendarSelectedFocus: '#16a595',
   calendarUnselectedBackground: '#ffffff',
   calendarUnselectedHoverActiveFocus: '#e7e7e7',
+  calendarSelectedButton: {
+    background: '#4db6ac',
+    hoverActive: '#35ada0',
+    focus: '#16a595'
+  },
+  calendarUnselectedButton: {
+    bacground: '#ffffff',
+    hoverActive: '#e7e7e7',
+    focus: '#e7e7e7'
+  },
   subtleGrey: '#707070', // used in view all button's border, footer description of donation cards
   loginBackground: '#212121',
   loginHoverActive: '#424242',
   loginFocus: '#616161',
-  categoryTagsFill: '#D9D9D9',
-  bump: '#4db6ac',
+  loginButton: {
+    background: '#212121',
+    hoverActive: '#424242',
+    focus: '#616161'
+  },
+  // categoryTagsFill: '#D9D9D9',
+  //bump: '#4db6ac',
+  bumpButton: { // TODO: change to primaryTeal
+    background: '#4db6ac'
+  },
   seeMoreHoverFocusActive: '#e7e7e7',
+  seeMoreButton: {
+    hoverActive: '#e7e7e7',
+    focus: '#e7e7e7'
+  },
   editProfileButtonBackground: '#43aad8',
   editProfileButtonHoverActive: '#31A2D6',
   editProfileButtonFocus: '#138CC8',
+  editProfileButton: {
+    background: '#43aad8',
+    hoverActive: '#31A2D6',
+    focus: '#138CC8'
+  },
   verifiedIconBackground: '#57A4FF',
+  verifiedIcon: {
+    background: '#57A4FF'
+  },
   chatButtonBackground: '#43aad8',
   chatButtonHoverActive: '#31A2D6',
   chatButtonFocus: '#138CC8',
+  chatButton: {
+    background: '#43aad8',
+    hoverActive: '#31A2D6',
+    focus: '#138CC8'
+  },
   chatSuggestDateButtonBackground: '#3B80ED',
   chatSuggestDateButtonHoverActive: '#3975D5',
   chatSuggestDateButtonFocus: '#3167BC',
+  chatSuggestDateButton: {
+    background: '#3B80ED',
+    hoverActive: '#3975D5',
+    focus: '#3167BC'
+  },
   chatCompleteButtonBackground: '#4DB6AC',
   chatCompleteButtonHoverActive: '#35ada0',
   chatCompleteButtonFocus: '#16a595',
+  chatCompletedButton: {
+    background: '#4DB6AC',
+    hoverActive: '#35ada0',
+    focus: '#16a595'
+  },
   chatSelected: '#3B80ED',
+  chatCardSelected: {
+    background: '#3B80ED'
+  },
   chatSeePostButtonBackground: '#707070',
   chatSeePostButtonHoverActive: '#616161',
   chatSeePostButtonFocus: '#424242',
+  chatSeePostButton: {
+    background: '#707070',
+    hoverActive: '#616161',
+    focus: '#424242'
+  },
   chatBorders: '#E8EDF1',
+  borders: {
+    background: '#E8EDF1'
+  },
   myChatBubbleBackground: '#3B80ED',
+  myChatBubble: {
+    background: '#3B80ED'
+  },
   oppositeChatBubbleBackground: '#EEEEEE',
-  ratingStarBackground: '#ED5555',
-  closedTagBackground: '#ED5555',
-  completedTagBackground: '#4DB6AC',
+  oppositeChatBubble: {
+    background: '#EEEEEE'
+  },
+  ratingStarBackground: '#ED5555', // TODO: change to primaryRed
+  closedTagBackground: '#ED5555',  // TODO: change to primaryRed
+  completedTagBackground: '#4DB6AC', // TODO: change to primaryTeal
+  primaryTeal: {
+    background: '#4DB6AC',
+    hoverActive: '#35ada0',
+    focus: '#16a595'
+  },
+  
   pendingTagBackground: '#C2C2C2',
-  saveChangesBackground: '#4db6ac',
-  saveChangesHoverActive: '#35ada0',
-  saveChangesFocus: '#16a595',
-  separatorBackground: '#eff2f5',
+  pendingTag: {
+    background: '#C2C2C2'
+  },
+  saveChangesBackground: '#4db6ac', // TODO: change to primaryTeal
+  saveChangesHoverActive: '#35ada0', // TODO: change to primaryTeal
+  saveChangesFocus: '#16a595', // TODO: change to primaryTeal
+  separatorBackground: '#eff2f5', 
+  separator: {
+    background: '#eff2f5'
+  },
   dragDropAccept: '#00e676',
   dragDropReject: '#ff1744',
   dragDropActive: '#2196f3',
@@ -53,20 +137,57 @@ export const colors = {
   dragDropText: '#bdbdbd',
   dragDropErrorText: '#d21c1c',
   dndBackgroundActive: '#CAD4DE',
+  dragAndDrop: {
+    accept: '#00e676',
+    reject: '#ff1744',
+    active: '#2196f3',
+    default: '#eeeeee',
+    background: '#fafafa',
+    text: '#bdbdbd',
+    errorText: '#d21c1c',
+    backgroundActive: '#CAD4DE'
+  },
   bannerBackground: '#ffffff',
+  white: '#ffffff',
   imageLoadingBackground: '#f0f1f1',
-  appreciationSelectedBackground: '#4db6ac',
-  appreciationSelectedHoverActive: '#35ada0',
-  appreciationSelectedFocus: '#16a595',
+  imageLoading: {
+    background: '#f0f1f1'
+  },
+  appreciationSelectedBackground: '#4db6ac', // TODO: change to primaryTeal
+  appreciationSelectedHoverActive: '#35ada0',// TODO: change to primaryTeal
+  appreciationSelectedFocus: '#16a595',// TODO: change to primaryTeal
   footerBackground: '#fafafa',
+  footer: {
+    background: '#fafafa'
+  },
   newsBackground: '#f3f4f5',
+  newsBar: {
+    background: '#f3f4f5'
+  },
   passwordCorrect: '#4caf50',
   passwordWrong: '#f44336',
+  passwordText: {
+    correct: '#4caf50',
+    wrong: '#f44336'
+  },
   chatScrollToBottomButtonBackground: '#3B80ED',
   chatScrollToBottomButtonHoverActive: '#3975D5',
   chatScrollToBottomButtonFocus: '#3167BC',
+  chatScrollToBottomButton: {
+    background: '#3B80ED',
+    hoverActive: '#3975D5',
+    focus: '#3167BC'
+  },
   categoryHover: '#eeeeee',
+  category: {
+    hover: '#eeeeee'
+  },
   nposCardBorder: '#d1d1d1',
   nposCardBorderHover: '#767676',
+  nposCard: {
+    border: '#d1d1d1',
+    borderHover: '#767676'
+  },
+  yellow: "#FFC620",
   yellowBackground: '#FFC620',
 };
