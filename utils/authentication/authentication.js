a66e3026da6b56c0a51971e4647c7f6ae2ce1e3f
Marcus Koh <kohchihao@gmail.com>
2020-12-02 22:27:09 +0800

    Refactor authentication code

--- utils/authentication/authentication.js
+++ utils/authentication/authentication.js
@@ -1,5 +1,7 @@
-import client from '../axios';
 import cookie from 'cookie';
+import admin from '@utils/admin-firebase';
+import AuthError from '@api/error/authError';
+import { deserializeFirestoreTimestampToUnixTimestampNode } from '@utils/firebase/deserializer';
 
 /**
  * Checks if a user is authenticated or not.
@@ -7,35 +9,62 @@ import cookie from 'cookie';
  *  res: The response from getServerSideProps
  */
 export async function isAuthenticated(req, res) {
+  const cookies = cookie.parse(req.headers.cookie || '');
+  const sessionCookie = cookies.session || '';
+  // Verify the session cookie. In this case an additional check is added to detect
+  // if the user's Firebase session was revoked, user deleted/disabled, etc.
   try {
-    const response = await client.get('/api/silentLogin', {
-      headers: {
-        cookie: req.headers.cookie,
+    let decodedClaims = await admin.auth().verifySessionCookie(sessionCookie, true /** checkRevoked */);
+    let currentUser = await admin.auth().getUser(decodedClaims.uid);
+    let user = await getUser(currentUser.customClaims, currentUser.uid);
+
+    // Checking for user type from 3 different sources because Cloud function doesnt update the claims fast enough.
+    const donor = decodedClaims?.donor || user?.user.donor || currentUser.customClaims?.donor || false;
+    const npo = decodedClaims?.npo || user?.npo || currentUser.customClaims?.npo || false;
+    const isClaimSet = currentUser.customClaims?.donor || currentUser.customClaims?.npo || false;
+
+    let data = {
+      user: {
+        ...user,
+        donor: donor,
+        npo: npo,
+        emailVerified: currentUser.emailVerified,
+        email: decodedClaims.email,
+        isClaimSet: isClaimSet,
       },
-    });
-    return response.data;
+    };
+    deserializeFirestoreTimestampToUnixTimestampNode(data);
+    return data;
   } catch (error) {
-    console.error(error.message);
+    console.error('silentLogin', error);
     return null;
   }
 }
 
-/**
- * Checks if a user is authenticated or not. If not, route them back to login page.
- *  req: The request from getServerSideProps
- *  res: The response from getServerSideProps
- */
-export async function isAuthenticatedFailureRouteBackToLogin(req, res) {
+async function getUser(decodedClaims, uid) {
   try {
-    const response = await client.get('/api/silentLogin', {
-      headers: {
-        cookie: req.headers.cookie,
-      },
-    });
-    return response.data;
+    if (decodedClaims && decodedClaims.donor) {
+      let doc = await admin.firestore().collection('donors').doc(uid).get();
+      return doc.data();
+    }
+
+    if (decodedClaims && decodedClaims.npo) {
+      let doc = await admin.firestore().collection('npos').doc(uid).get();
+      return doc.data();
+    }
+
+    let doc = await admin.firestore().collection('donors').doc(uid).get();
+    if (doc.exists) {
+      return doc.data();
+    } else {
+      let doc = await admin.firestore().collection('npos').doc(uid).get();
+      return doc.data();
+    }
   } catch (error) {
-    res.writeHead(302, { Location: '/login' });
-    res.end();
-    return null;
+    if (error instanceof AuthError) {
+      throw new AuthError('user-does-not-exist', 'User does not exists');
+    } else {
+      throw new Error('Unknown error in getUser');
+    }
   }
 }
--- utils/firebase/deserializer.js
+++ utils/firebase/deserializer.js
@@ -1,4 +1,5 @@
 import { firebase } from '@utils/firebase';
+import admin from '@utils/admin-firebase';
 
 export const deserializeFirestoreTimestampToUnixTimestamp = (...objects) => {
   for (let object of objects) {
@@ -7,6 +8,18 @@ export const deserializeFirestoreTimestampToUnixTimestamp = (...objects) => {
         object[key] = value.toMillis();
       } else if (value instanceof Object) {
         deserializeFirestoreTimestampToUnixTimestamp(value);
+      }
+    }
+  }
+};
+
+export const deserializeFirestoreTimestampToUnixTimestampNode = (...objects) => {
+  for (let object of objects) {
+    for (let [key, value] of Object.entries(object)) {
+      if (value instanceof admin.firestore.Timestamp) {
+        object[key] = value.toMillis();
+      } else if (value instanceof Object) {
+        deserializeFirestoreTimestampToUnixTimestampNode(value);
       }
     }
   }
