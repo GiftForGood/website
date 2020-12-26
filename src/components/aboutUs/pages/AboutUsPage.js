55b0baa3174e9bbfd05f1c571a09b8bec71fc338
Marcus Koh <kohchihao@gmail.com>
2020-12-26 16:57:08 +0800

    Add CTA section

--- src/components/aboutUs/pages/AboutUsPage.js
+++ src/components/aboutUs/pages/AboutUsPage.js
@@ -1,18 +1,16 @@
 import React from 'react';
-import styled, { css } from 'styled-components';
-import media from '@kiwicom/orbit-components/lib/utils/mediaQuery';
 import Banner from '../modules/Banner';
-import TeamDescription from '../modules/TeamDescription';
-import { Heading, Alert, Stack } from '@kiwicom/orbit-components/lib';
 import HowItWorks from '@components/aboutUs/modules/HowItWorks';
 import Stats from '@components/aboutUs/modules/Stats';
+import CallToAction from '@components/aboutUs/modules/CallToAction'
+
 const AboutUsPage = () => {
   return (
     <>
       <Banner />
       <HowItWorks />
       <Stats />
-      <TeamDescription />
+      <CallToAction />
     </>
   );
 };
